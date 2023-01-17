ac210d35a6c4d8a12c2afca05b126fe46f64ae9d
Semen Nebogatov <semen.neb@gmail.com>
2023-01-17 22:34:28 +0700

    update index.ts with game-lobbies & types + lint

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,33 @@
-import { Server } from "socket.io";
-import dotenv from "dotenv";
+import { Server, Socket } from "socket.io";
 import { instrument } from "@socket.io/admin-ui";
-import { Socket } from "socket.io";
-import globalChatHandler from "./namespaces/global-chat";
+import dotenv from "dotenv";
 import serverOptions from "./server-options";
+
+import { IO } from "./index.types";
+
+import { GlobalChatIO } from "./namespaces/global-chat/global-chat.types";
+import { GameLobbiesIO } from "./namespaces/game-lobbies/game-lobbies.types";
+
+import globalChatHandler from "./namespaces/global-chat/global-chat.handler";
+import gameLobbiesHandler from "./namespaces/game-lobbies/game-lobbies.handler";
+
 dotenv.config();
 
 const port = Number(process.env.PORT);
 
-const io = new Server(port, serverOptions);
+const io: IO.ServerIO = new Server(port, serverOptions);
 
 instrument(io, { auth: false, mode: "development" });
 
-io.on("connection", (socket: Socket) => {
+io.on("connect", (socket: Socket) => {
   console.log(`Hi ${socket.id}`);
   socket.on("disconnect", () => console.log(`Bye ${socket.id}`));
 });
 
-export const globalChat = io.of("/global-chat");
+export const globalChat: GlobalChatIO.NamespaceIO = io.of("/global-chat");
 
 globalChat.on("connect", globalChatHandler);
 
+export const gameLobbies: GameLobbiesIO.NamespaceIO = io.of("/game-lobbies");
+
+gameLobbies.on("connect", gameLobbiesHandler);
