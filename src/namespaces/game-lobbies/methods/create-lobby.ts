99bd31825e293da432421edd827e01ddeed0930d
GeForsen- <semen.neb@gmail.com>
2023-01-22 14:25:50 +0700

    update initiate-lobby-creation to create-lobby

---
+++ src/namespaces/game-lobbies/methods/create-lobby.ts
@@ -0,0 +1,40 @@
+import { gameLobbies } from "../../../index";
+import { lobbies } from "../game-lobbies.handler";
+import generateNotificationFromError from "../../../utils/generate-notification-from-error";
+import { GameLobbiesIO, LobbySettings } from "../game-lobbies.types";
+import DB from "../../../db";
+import handleUserAlreadyInLobby from "./handle-user-already-in-lobby";
+import Lobby from "../lobby";
+import tryAuth from "../../../checkers/try-auth";
+
+export default function createLobby(
+  this: { socket: GameLobbiesIO.SocketIO },
+  gameLobbySettings: LobbySettings,
+) {
+  try {
+    const { accName } = tryAuth(this.socket).data;
+    const user = DB.User.findByAccNameOrThrow(accName!);
+
+    handleUserAlreadyInLobby(accName!);
+
+    const lobby = new Lobby({ adminAccName: accName!, settings: gameLobbySettings });
+    gameLobbies.emit("lobbyCreated", lobby);
+
+    lobby.addUser(user);
+    gameLobbies.emit("addedUser", user, lobby.id);
+
+    lobbies.push(lobby);
+  } catch (error) {
+    if (error instanceof Error) {
+      const notification = generateNotificationFromError(error);
+      gameLobbies.to(this.socket.id).emit("sendNotification", notification);
+    } else {
+      console.log("GlobalChat CreateLobby Error:", error);
+    }
+  }
+}
+
+
+
+
+
