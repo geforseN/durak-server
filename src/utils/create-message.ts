36f3cd6956c84774c133bfe5b4c8f9e4b9dabe36
GeForsen- <semen.neb@gmail.com>
2023-01-27 23:05:33 +0700

    update createMessage types

--- src/utils/create-message.ts
+++ src/utils/create-message.ts
@@ -1,7 +1,7 @@
-import { User } from "../db/user";
-import { GlobalChatDB } from "../db/global-chat";
+import { GlobalChatDB } from "../namespaces/global-chat/global-chat.namespace";
+import { LobbyUser } from "../namespaces/game-lobbies/entity/lobby-users";
 
-export default function createMessage(user: User, text: string): GlobalChatDB.Message {
+export default function createMessage(user: LobbyUser, text: string): GlobalChatDB.Message {
   return {
     sender: user,
     date: new Date().getTime(),
