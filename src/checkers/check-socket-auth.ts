3a03d418f2833af4679d50885907cf9072b07636
GeForsen- <semen.neb@gmail.com>
2023-01-27 22:51:53 +0700

    remove unused helper functions

--- src/checkers/check-socket-auth.ts
+++
@@ -1,5 +0,0 @@
-import { Socket } from "socket.io";
-
-export default function checkSocketAuth(socket: Socket): void | never {
-  if (!socket.data.accName) throw new Error("Не найден пользователь");
-}
--- src/checkers/try-get-cookie.ts
+++
@@ -1,9 +0,0 @@
-import { Socket } from "socket.io";
-
-export default function tryGetCookie(socket: Socket): string | never{
-  const { cookie } = socket.handshake.headers;
-
-  if (!cookie) throw new Error("Не удалось получить cookie");
-
-  return cookie;
-}
