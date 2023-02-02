bcdf84348e82efa244eb4eced617276338635b95
GeForsen- <semen.neb@gmail.com>
2023-02-02 13:33:06 +0700

    Talon: rename drawCardsFromTop to popCards, shuffle return this, del shuffle from constructor

--- src/durak-game/entity/Deck/Talon.ts
+++ src/durak-game/entity/Deck/Talon.ts
@@ -5,19 +5,19 @@ import { randomInt } from "crypto";
 export default class Talon extends Deck {
   constructor(size: CardCount) {
     super(size);
-    this.shuffle();
   }
 
   get trumpCard(): Card {
     return this._value[0];
   }
 
-  shuffle(): void {
+  shuffle(): this {
     for (let i = 0; i < this.count; i++) {
       let j = randomInt(this.count);
       while (i === j) j = randomInt(this.count);
       [this._value[j], this._value[i]] = [this._value[i], this._value[j]]
     }
+    return this;
   }
 
   ensureCanDraw(cardCount: number): this | never {
@@ -28,9 +28,8 @@ export default class Talon extends Deck {
     return this;
   }
 
-  drawCardsFromTop(cardCount = 1): Card[] {
+  popCards(cardCount = 1): Card[] {
     const index = this.count - 1 - cardCount;
     return this._value.splice(index, cardCount);
   }
-
 }
