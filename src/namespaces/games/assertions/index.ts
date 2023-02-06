f78f9725edd3911e23582d048fabd6156a1006d5
GeForsen- <semen.neb@gmail.com>
2023-02-06 22:02:45 +0700

    delete index file of game assert functions

--- src/namespaces/games/assertions/index.ts
+++
@@ -1,7 +0,0 @@
-import assertBeforeAttack from "./assert.before-attack";
-import assertSocketHasAccess from "./assert.socket-has-access";
-import assertDefenderRole from "./assert.defender-role";
-import assertPlayerHasCard from "./assert.player-has-card";
-
-
-export { assertBeforeAttack, assertSocketHasAccess, assertPlayerHasCard, assertDefenderRole };
