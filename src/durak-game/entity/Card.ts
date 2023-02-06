d0b201f0b544d9357ba55eab4af6c74845dd7b0b
GeForsen- <semen.neb@gmail.com>
2023-02-06 21:52:52 +0700

    Card: add isTrump method & lint

--- src/durak-game/entity/Card.ts
+++ src/durak-game/entity/Card.ts
@@ -1,15 +1,20 @@
 import { Power, powerRecord, Rank, Suit } from "../utility.durak";
+import DurakGame from "../durak-game";
 
-export type CardConstructor = {suit: Suit, rank: Rank};
+export type CardConstructor = { suit: Suit, rank: Rank };
 
 export default class Card {
-  rank: Rank
-  suit: Suit
-  power: Power
+  rank: Rank;
+  suit: Suit;
+  power: Power;
 
-  constructor({rank, suit}: CardConstructor) {
+  constructor({ rank, suit }: CardConstructor) {
     this.rank = rank;
     this.suit = suit;
     this.power = powerRecord[rank];
+  }
+
+  isTrump({ game }: { game: DurakGame }): boolean {
+    return game.talon.trumpCard.suit === this.suit;
   }
 }
