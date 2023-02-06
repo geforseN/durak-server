3e7c68b5629405ce87aa3e3d6d56d62467008b11
GeForsen- <semen.neb@gmail.com>
2023-02-06 13:39:06 +0700

    CardPlayer concept

--- src/durak-game/entity/card-player.ts
+++ src/durak-game/entity/card-player.ts
@@ -2,19 +2,39 @@ import { LobbyUser } from "../../namespaces/lobbies/entity/lobby-users";
 import Hand from "./Deck/Hand";
 import Card from "./card";
 
+export type CardPlayerRole =  "DEFENDER" | "ATTACKER" | "NONE"
+export type CardPlayerStatus = "WAITING OPPONENT"| "WAITING ANOTHER ATTACKER" | "ATTACKING" | "DEFENDING" | "NONE";
+
 export default class CardPlayer {
-  // { accname, nickname, connectStatus, personalLink, photoUrl }
-  info: LobbyUser & { isConnected: boolean };
+  info: LobbyUser;
   hand: Hand;
+  status: CardPlayerStatus;
+  role: CardPlayerRole;
   left?: CardPlayer;
   right?: CardPlayer;
 
   constructor(lobbyUser: LobbyUser) {
-    this.info = { ...lobbyUser, isConnected: false };
+    this.info = lobbyUser;
     this.hand = new Hand();
+    this.status = "NONE";
+    this.role = "NONE";
   }
 
   receiveCards(...cardsToReceive: Card[]): void {
     this.hand.receiveCards(...cardsToReceive);
+  }
+
+  makeDefender(): this {
+    this.role = "DEFENDER";
+    return this;
+  }
+
+  makeAttacker(): this {
+    this.role = "ATTACKER";
+    return this;
+  }
+
+  get missingCardNumber(): number {
+    return 6 - this.hand.count;
   }
 }
