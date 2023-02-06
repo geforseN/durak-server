32f916f8ac9468fc36a4c43368bb29c84db2d496
GeForsen- <semen.neb@gmail.com>
2023-02-06 13:44:08 +0700

    add index file for game assert functions

---
+++ src/namespaces/games/assertions/index.ts
@@ -0,0 +1,7 @@
+import assertBeforeAttack from "./assert.before-attack";
+import assertSocketHasAccess from "./assert.socket-has-access";
+import assertDefenderRole from "./assert.defender-role";
+import assertPlayerHasCard from "./assert.player-has-card";
+
+
+export { assertBeforeAttack, assertSocketHasAccess, assertPlayerHasCard, assertDefenderRole };
