fda5181d238f5c1687e6874e1f20d5981a3c03d4
GeForsen- <semen.neb@gmail.com>
2023-02-08 00:20:04 +0700

    add removeCard + make handleCardInsert remove card with emits

--- src/durak-game/entity/Players/Attacker.ts
+++ src/durak-game/entity/Players/Attacker.ts
@@ -1,16 +1,29 @@
 import Player from "./Player";
 import { CardInsert, PlaceCardData } from "../../../namespaces/games/methods/handle-insert-card-on-desk";
+import { GamesIO } from "../../../namespaces/games/games.types";
+import Card from "../Card";
 
 export default class Attacker extends Player implements CardInsert {
   constructor(player: Player) {
     super(player);
   }
 
-  handleCardInsert({ game, card, slot, slotIndex }: PlaceCardData): void | never {
+  handleCardInsert(
+    { game, card, slot, slotIndex, socket }: PlaceCardData & { socket: GamesIO.SocketIO },
+  ): void | never {
     const { desk } = game;
-    if (desk.isEmpty) return game.insertAttackCard({ card, index: slotIndex });
-    if (slot.hasAttackCard) throw new Error("Занято");
-    if (!desk.hasCardWithRank(card.rank)) throw new Error("Нет схожего ранга на доске");
-    desk.insertAttackerCard({ index: slotIndex, card });
+    if (!desk.isEmpty) {
+      if (slot.attackCard) throw new Error("Занято");
+      if (!desk.hasCardWithRank(card.rank)) throw new Error("Нет схожего ранга на доске");
+    }
+    this.removeCard(card);
+    socket.to(socket.data.accname as string).emit("self__removeCard", card);
+    game.gameService.changeCardCount({ accname: socket.data.accname as string, cardCount: this.hand.count, socket });
+    game.insertAttackCardOnDesk({ index: slotIndex, card, socket });
+  }
+
+  removeCard(card: Card): void {
+    const index = this.hand.findIndex({ card });
+    this.hand.value.splice(index, 1);
   }
 }
--- src/durak-game/entity/Players/Defender.ts
+++ src/durak-game/entity/Players/Defender.ts
@@ -1,23 +1,36 @@
 import Player from "./Player";
 import { CardInsert, PlaceCardData } from "../../../namespaces/games/methods/handle-insert-card-on-desk";
+import { GamesIO } from "../../../namespaces/games/games.types";
+import Card from "../Card";
 
 export default class Defender extends Player implements CardInsert {
   constructor(player: Player) {
     super(player);
   }
 
-  handleCardInsert({ game, slot, slotIndex, card }: PlaceCardData): void | never {
-    if (slot.hasDefendCard) throw new Error("Карта уже побита");
+  handleCardInsert(
+    { game, slot, slotIndex, card, socket }: PlaceCardData & { socket: GamesIO.SocketIO },
+  ): void | never {
+    if (slot.defendCard) throw new Error("Карта уже побита");
     if (!slot.attackCard) throw new Error("Нет от чего защищаться");
 
-    if (slot.hasTrumpAttackCard({ game })) {
-      if (!card.isTrump({ game })) throw new Error("Козырную карту можно побить только козырной");
+    const { trumpSuit } = game.talon;
+
+    if (slot.attackCard.suit === trumpSuit) {
+      if (card.suit !== trumpSuit) throw new Error("Козырную карту можно побить только козырной");
       if (card.power < slot.attackCard.power) throw new Error("Вы кинули слабую карту");
-    } else if (!card.isTrump({ game })) {
+    } else if (card.suit !== trumpSuit) {
       if (card.suit !== slot.attackCard.suit) throw new Error("Вы кинули неверню масть");
       if (card.power < slot.attackCard.power) throw new Error("Вы кинули слабую карту");
     }
+    this.removeCard(card);
+    socket.to(socket.data.accname as string).emit("self__removeCard", card);
+    game.gameService.changeCardCount({ accname: socket.data.accname as string, cardCount: this.hand.count, socket });
+    game.insertDefendCardOnDesk({ card, index: slotIndex, socket });
+  }
 
-    game.insertDefendCard({ card, index: slotIndex });
+  removeCard(card: Card): void {
+    const index = this.hand.findIndex({ card });
+    this.hand.value.splice(index, 1);
   }
 }
