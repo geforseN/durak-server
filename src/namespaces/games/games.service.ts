5bba739af50bd6593f632cd1533e5e315a611527
GeForsen- <semen.neb@gmail.com>
2023-02-08 23:42:20 +0700

    GamesService: refactor constructor, add chain this

--- src/namespaces/games/games.service.ts
+++ src/namespaces/games/games.service.ts
@@ -1,15 +1,11 @@
 import { GamesIO } from "./games.types";
-import { GameId, io } from "../../index";
 import { LobbyUserIdentifier } from "../lobbies/entity/lobby-users";
 import Card from "../../durak-game/entity/Card";
 
 export type GameSocket = { socket: GamesIO.SocketIO };
 
 export class GamesService {
-  private namespace: GamesIO.NamespaceIO;
-
-  constructor(gameId: GameId) {
-    this.namespace = io.of(gameId);
+  constructor(private namespace: GamesIO.NamespaceIO) {
   }
 
   insertAttackCard({ card, index, socket }: { card: Card, index: number } & GameSocket) {
@@ -28,19 +24,23 @@ export class GamesService {
     socket.to(accname).emit("self__removeCard", card);
   }
 
-  hideAttackUI({ accname }: LobbyUserIdentifier) {
+  hideAttackUI({ accname }: LobbyUserIdentifier): this {
     this.namespace.to(accname).emit("attackUI__shouldShow", false);
+    return this;
   }
 
-  revealAttackUI({ accname }: LobbyUserIdentifier) {
+  revealAttackUI({ accname }: LobbyUserIdentifier): this {
     this.namespace.to(accname).emit("attackUI__shouldShow", true);
+    return this;
   }
 
-  hideDefendUI({ accname }: LobbyUserIdentifier) {
+  hideDefendUI({ accname }: LobbyUserIdentifier): this {
     this.namespace.to(accname).emit("defendUI__shouldShow", false);
+    return this;
   }
 
-  revealDefendUI({ accname }: LobbyUserIdentifier) {
+  revealDefendUI({ accname }: LobbyUserIdentifier): this {
     this.namespace.to(accname).emit("defendUI__shouldShow", true);
+    return this;
   }
 }
