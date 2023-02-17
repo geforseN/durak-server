c34f783fd7c44264abb8c4de6855b87f58e955a3
GeForsen- <semen.neb@gmail.com>
2023-02-17 17:05:07 +0700

    Player: remove status, rename ICardInsert into ICardPut, add IMoveStop, lint

--- src/durak-game/entity/Players/Player.ts
+++ src/durak-game/entity/Players/Player.ts
@@ -1,37 +1,37 @@
 import { LobbyUser } from "../../../namespaces/lobbies/entity/lobby-users";
 import Hand from "../Deck/Hand";
 import Card from "../Card";
-import { PlaceCardData } from "../../../namespaces/games/methods/handle-insert-card-on-desk";
+import { PlaceCardData } from "../../../namespaces/games/methods/handle-put-card-on-desk";
 import { GameSocket } from "../../../namespaces/games/game.service";
+import DurakGame from "../../durak-game";
 
-export type CardPlayerRole = "DEFENDER" | "ATTACKER" | "NONE"
-export type CardPlayerStatus = "WAITING OPPONENT" | "WAITING ANOTHER ATTACKER" | "ATTACKING" | "DEFENDING" | "NONE";
-
-export interface CardInsert {
-  handleCardInsert({ game, card, slot, slotIndex, socket }: PlaceCardData & GameSocket): void | never;
+export interface CardPut {
+  putCardOnDesk({ game, card, slotIndex, socket }: PlaceCardData & GameSocket): void | never;
 }
 
 export interface CardRemove {
-  removeCard(card: Card): void;
+  removeCard(card: Card): void | never;
+}
+
+export interface MoveStop {
+  stopMove({ game }: { game: DurakGame }): void | never;
 }
 
 export default class Player {
   info!: LobbyUser;
   hand!: Hand;
-  status!: CardPlayerStatus;
   left!: Player;
   right!: Player;
-  index!: number
+  index!: number;
 
   constructor(lobbyUser: LobbyUser | Player) {
     if (lobbyUser instanceof Player) {
       const player = lobbyUser;
       this.hand = player.hand;
-      this.status = player.status;
       this.info = player.info;
       this.left = player.left;
       this.right = player.right;
-      this.index = player.index
+      this.index = player.index;
     } else this.initialize(lobbyUser);
   }
 
@@ -46,6 +46,5 @@ export default class Player {
   private initialize(lobbyUser: LobbyUser) {
     this.info = lobbyUser;
     this.hand = new Hand();
-    this.status = "NONE";
   }
 }
