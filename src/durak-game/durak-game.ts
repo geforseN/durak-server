8f07f259e5710e91a31dce92eacc4bd8b2ccda09
GeForsen- <semen.neb@gmail.com>
2023-02-17 17:24:18 +0700

    update DurakGame, add GameNamespace types

--- src/durak-game/durak-game.ts
+++ src/durak-game/durak-game.ts
@@ -6,29 +6,28 @@ import Talon from "./entity/Deck/Talon";
 import Desk from "./entity/Desk";
 import { LobbyUserIdentifier } from "../namespaces/lobbies/entity/lobby-users";
 import { GameSocket, GameService } from "../namespaces/games/game.service";
-import { GamesIO, GameState } from "../namespaces/games/games.types";
+import { GamesIO } from "../namespaces/games/games.types";
 import Card from "./entity/Card";
 import Attacker from "./entity/Players/Attacker";
 import Defender from "./entity/Players/Defender";
 import Player, { CardRemove } from "./entity/Players/Player";
+import GameRound from "./GameRound";
 
-export type GameStat = { roundNumber: number };
-export type GameInfo = { id: string, adminAccname: string | null };
+export type GameInfo = { id: string, adminAccname: string };
 
 export default class DurakGame {
   info: GameInfo;
-  stat: GameStat;
   settings: GameSettings;
   players: Players;
   talon: Talon;
   discard: Discard;
   desk: Desk;
   service!: GameService;
+  round!: GameRound;
 
   constructor({ id, settings, users, adminAccname }: Lobby) {
-    this.info = { id, adminAccname };
+    this.info = { id, adminAccname: adminAccname! };
     this.settings = settings;
-    this.stat = { roundNumber: 0 }; // new GameStat
     this.players = new Players(users);
     this.talon = new Talon(settings.cardCount);
     this.discard = new Discard();
@@ -37,11 +36,11 @@ export default class DurakGame {
 
   start(namespace: GamesIO.NamespaceIO) {
     this.service = new GameService(namespace);
-    this.makeFirstDistributionByOne();
-    this.stat.roundNumber++;
-    const { attacker, defender } = this.makeInitialDefenderAndAttacker();
-    this.service.revealAttackUI({ accname: attacker.info.accname });
-    this.service.revealDefendUI({ accname: defender.info.accname });
+    this.makeFirstDistribution({ howMany: 1 });
+    const attacker = this.makeInitialAttacker();
+    const defender = this.makeDefender(attacker.left);
+    this.round = new GameRound({ attacker, defender });
+    this.service.setAttackUI("revealed", attacker);
   }
 
   insertAttackCardOnDesk({ card, index, socket }: { card: Card, index: number } & GameSocket): void {
@@ -57,44 +56,88 @@ export default class DurakGame {
   removeCard({ player, socket, card }: { player: Player & CardRemove, card: Card } & GameSocket) {
     const { info: { accname } } = player;
     player.removeCard(card);
-    this.service.removeCard({ accname, card, socket });
-    this.service.changeCardCount({ accname, socket, cardCount: player.hand.count });
+    this.service
+      .removeCard({ accname, card, socket })
+      .changeCardCount({ accname, socket, cardCount: player.hand.count });
   }
 
-  // делаем интервал
-  // даем возможность кидать карты
-  // при конце хода очищаем интервал и
-  // выдаем право хода защищающемуся
-
-  // защищающийся пытается отбится
-  //
-  // при успешной защите можно:
-  // - ЛИБО повторить заход атаки
-  //   - спросить у первого атаковавшего
-  //   - если не может кинуть карты, то спрашиваем остальных слева
-  // - ЛИБО перевести карты в биту
-  //
-  // при провале:
-  // - неотбившийся забирает карты
-  // - ход достается левому от проигравшего
-
-  restoreState({ accname }: LobbyUserIdentifier): GameState {
-    // return new GameState({ accname });
-    const self = this.players.getSelf({ accname });
-    const enemies = this.players.getEnemies({ accname });
-    const desk = this.desk.slots;
-    return { self, enemies, desk };
+  clearDesk() {
+    this.desk.clear();
+    this.service.clearDesk();
   }
 
-  private makeFirstDistributionByOne() {
-    const cardCount = this.players.count * 6;
-    const cards = this.talon.shuffle().popCards(cardCount);
-    this.players.receiveCardsByOne(cards);
+  handleBadDefense({ defender }: { defender: Defender }): void {
+    this.lostRound({ defender });
+    return this.handleNewTurn({ nextAttacker: defender.left });
   }
 
-  private makeInitialDefenderAndAttacker(): { attacker: Attacker, defender: Defender } {
-    const attacker = this.players.makeAttacker({ accname: this.info.adminAccname! });
-    const defender = this.players.makeDefender({ accname: attacker.left.info.accname });
-    return { attacker, defender };
+  handleSuccesfullDefense({ defender }: { defender: Defender }): void {
+    this.wonRound({ defender });
+    return this.handleNewTurn({ nextAttacker: defender });
+  }
+
+  handleNewTurn({ nextAttacker }: { nextAttacker: Player }) {
+    this.makeCardDistribution();
+    const attacker = this.makeAttacker(nextAttacker);
+    const defender = this.makeDefender(attacker.left);
+    this.round.nextRound({ attacker, defender });
+    this.service.setAttackUI("revealed", attacker);
+  }
+
+  makeCardDistribution() {
+    for (const accname of this.round.distributionAccnameQueue) {
+      if (this.talon.isEmpty) break;
+      const player = this.players.tryGetPlayer({ accname });
+      this.pushCardsFromTalon({ player });
+    }
+  }
+
+  private pushCardsFromTalon({ player }: { player: Player }) {
+    const cards = this.talon.popCards(player.missingNumberOfCards);
+    player.receiveCards(...cards);
+    this.service.pushFromTalon({ player, cards });
+    if (this.talon.isEmpty) this.service.moveTrumpCard({ receiver: player });
+  }
+
+  private lostRound({ defender }: { defender: Defender }) {
+    defender.receiveCards(...this.desk.cards);
+    this.service.lostRound({ defender });
+    this.clearDesk();
+  }
+
+  private wonRound({ defender }: { defender: Defender }) {
+    this.discard.push(...this.desk.cards);
+    this.service.wonRound({ defender });
+    this.clearDesk();
+  }
+
+  restoreState({ accname, socket }: LobbyUserIdentifier & GameSocket): void {
+    this.service.restoreState({ accname, socket, game: this });
+  }
+
+  private makeFirstDistribution({ howMany }: { howMany: number }) {
+    const cardCountToDistribute = this.players.count * 6;
+    const talonCards = this.talon.shuffle().popCards(cardCountToDistribute);
+    this.players.receiveCards({ talonCards, howMany });
+  }
+
+  private makeInitialAttacker(): Attacker {
+    return this.makeAttacker({ accname: this.info.adminAccname });
+  }
+
+  makeAttacker(playerOrIdentifier: Player | LobbyUserIdentifier): Attacker {
+    const attacker = this.players.makeAttacker(playerOrIdentifier);
+    this.service.changeRoleTo("attacker", attacker);
+    return attacker;
+  }
+
+  makeDefender(playerOrIdentifier: Player | LobbyUserIdentifier): Attacker {
+    const defender = this.players.makeDefender(playerOrIdentifier);
+    this.service.changeRoleTo("defender", defender);
+    return defender;
+  }
+
+  get isOver() {
+    return this.players.__value.filter((player) => player.hand.count !== 0).length === 1;
   }
 }
--- src/namespaces/games/games.types.ts
+++ src/namespaces/games/games.types.ts
@@ -1,13 +1,18 @@
 import { Namespace, Socket } from "socket.io";
 import Card from "../../durak-game/entity/Card";
 import { Suit } from "../../durak-game/utility.durak";
-import { CardPlayerRole, CardPlayerStatus } from "../../durak-game/entity/Players/Player";
 import DeskSlot from "../../durak-game/entity/DeskSlot";
 import Self from "../../durak-game/DTO/Self.dto";
 import Enemy from "../../durak-game/DTO/Enemy.dto";
 import NotificationAlert from "../../module/notification-alert";
 
-export type GameState = { self: Self, enemies: Enemy[], desk: DeskSlot[] }
+export type GameState = { self: Self, enemies: Enemy[], deskSlots: DeskSlot[] };
+
+export type SocketResponse = { status: "ATT" | "DEF" } | { status: "NOK", message: string };
+export type ResponseCallback = (res: SocketResponse) => void;
+
+export type PlayerRole = "defender" | "attacker" | "player";
+export type UIStatus = "revealed" | "hidden" | "freeze";
 
 export namespace GamesIO {
   export type ClientToServerEvents = {
@@ -21,23 +26,24 @@ export namespace GamesIO {
     "state__restore": (state: GameState) => void;
     "notification__send": (notification: NotificationAlert) => void;
 
-    "talon__distributeCards": () => void;
+    "talon__distributeCards": (accname: string, cardCountToDistribute: number) => void;
     "talon__showTrumpCard": (trumpCard: Card) => void;
+    "talon__moveTrumpCardTo": (accname: string) => void;
 
-    "desk__pushCardsInDiscard": () => void;
+    "defendUI__setStatus": (status: UIStatus) => void;
+    "attackUI__setStatus": (status: UIStatus) => void;
 
-    "role__update": (accname: string, role: CardPlayerRole) => void;
-    "status__update": (accname: string, status: CardPlayerStatus) => void;
-    "defendUI__shouldShow": (shouldShow: boolean) => void;
-    "attackUI__shouldShow": (shouldShow: boolean) => void;
-
+    "player__changeRole": (role: PlayerRole, accname: string) => void;
+    "player__receiveCards": (cards: Card[]) => void;
     "self__removeCard": (card: Card) => void;
-    "enemies__changeCardCount": (accname: string, cardCount: number) => void;
+    "enemy__changeCardCount": (accname: string, cardCount: number) => void;
+    "defender__lostRound": (accname: string) => void;
+    "defender__wonRound": (accname: string) => void;
 
     "desk__clear": () => void;
     "desk__insertAttackCard": (card: Card, slotIndex: number) => void;
     "desk__insertDefendCard": (card: Card, slotIndex: number) => void;
-    "": () => void;
+    "desk__pushToDiscard": () => void;
   }
 
   export type InterServerEvents = {}
@@ -70,7 +76,3 @@ export type AdditionalGameState = {
   trumpSuit: Suit;
   trump?: Card;
 }
-
-
-// ClientToServer "talon__distributeCards": () => void
-// ClientToServer "discard__pushCards": () => void
