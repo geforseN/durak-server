0b0371e92f64b2b7f61e0b708eb082e035fc75b3
GeForsen- <semen.neb@gmail.com>
2023-02-17 17:18:20 +0700

    handlePutCardOnDesk: type callback & clean

--- src/namespaces/games/methods/handle-put-card-on-desk.ts
+++ src/namespaces/games/methods/handle-put-card-on-desk.ts
@@ -1,25 +1,21 @@
 import Card from "../../../durak-game/entity/Card";
 import DurakGame from "../../../durak-game/durak-game";
-import DeskSlot from "../../../durak-game/entity/DeskSlot";
 import { GameSocket } from "../game.service";
+import { ResponseCallback } from "../games.types";
 
-export type SlotInfo = { slot: DeskSlot, slotIndex: number };
-export type PlaceCardData = { game: DurakGame, card: Card } & SlotInfo;
+export type PlaceCardData = { game: DurakGame, card: Card, slotIndex: number };
 
 export default function handlePutCardOnDesk(
   this: { game: DurakGame, accname: string } & GameSocket,
   card: Card,
   slotIndex: number,
-  callback: any,
+  callback: ResponseCallback,
 ) {
   const { game, socket, accname } = this;
   if (!accname) throw new Error("Вы вне игры");
 
   const player = game.players.tryGetPlayer({ accname });
-
   if (!player.hand.has({ card })) throw new Error("Нет такой карты");
-
-  const slot = game.desk.getSlot({ index: slotIndex });
 
   if (game.players.isDefender(player) || game.players.isAttacker(player)) {
     player.putCardOnDesk({ game, slotIndex, card, socket });
