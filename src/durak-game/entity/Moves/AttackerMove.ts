ce601f53590b685e3159439ee120629242d5c655
GeForsen- <semen.neb@gmail.com>
2023-03-01 01:35:28 +0700

    AttackerMove: add type

--- src/durak-game/entity/Moves/AttackerMove.ts
+++ src/durak-game/entity/Moves/AttackerMove.ts
@@ -1,6 +1,8 @@
 import Attacker from "../Players/Attacker";
 import { GameMove, GameMoveConstructorArgs } from "./GameMove";
 
+export type AttackerMoveConstructorArgs = GameMoveConstructorArgs<Attacker>;
+
 export class AttackerMove extends GameMove {
   constructor({ number, allowedPlayer, deskCardCount }: GameMoveConstructorArgs<Attacker>) {
     super({ number, allowedPlayer, deskCardCount });
