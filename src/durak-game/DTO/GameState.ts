2811d24895256fac12278b2fdfc76acc8069b4fe
GeForsen- <semen.neb@gmail.com>
2023-03-01 02:49:34 +0700

    GameStateDTO: add allowedPlayerAccname

--- src/durak-game/DTO/GameState.ts
+++ src/durak-game/DTO/GameState.ts
@@ -10,11 +10,13 @@ export default class GameState {
   enemies: Enemy[];
   deskSlots: DeskSlot[];
   trumpCard: Card;
+  allowedPlayerAccname: string
 
   constructor(game: DurakGame, accname: LobbyUserIdentifier["accname"]) {
     this.self = new Self(game.players.tryGetPlayer({ accname }));
     this.enemies = game.players.getPlayerEnemies({ accname }).map(((enemy) => new Enemy(enemy)));
     this.deskSlots = game.desk.slots;
     this.trumpCard = game.talon.trumpCard;
+    this.allowedPlayerAccname = game.round.currentMove.allowedPlayerAccname;
   }
 }
