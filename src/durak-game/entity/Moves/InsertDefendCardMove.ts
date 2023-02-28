181096be501ccd2a5d2f0695f4f2a0786de34463
GeForsen- <semen.neb@gmail.com>
2023-03-01 01:24:45 +0700

    InsertDefendCardMove: add card & slotIndex prop

--- src/durak-game/entity/Moves/InsertDefendCardMove.ts
+++ src/durak-game/entity/Moves/InsertDefendCardMove.ts
@@ -1,7 +1,15 @@
 import { DefenderMove, DefenderMoveConstructorArgs } from "./DefenderMove";
+import Card from "../Card";
+
+type InsertDefendCardMoveConstructorArgs = { card: Card, slotIndex: number } & DefenderMoveConstructorArgs;
 
 export class InsertDefendCardMove extends DefenderMove {
-  constructor({ number, allowedPlayer, deskCardCount }: DefenderMoveConstructorArgs) {
+  card: Card;
+  slotIndex: number;
+
+  constructor({ number, allowedPlayer, deskCardCount, ...args }: InsertDefendCardMoveConstructorArgs) {
     super({ number, allowedPlayer, deskCardCount });
+    this.slotIndex = args.slotIndex;
+    this.card = args.card;
   }
 }
