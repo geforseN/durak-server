cb662560f95b7ffac9acbfa31bdf135c3c477f90
GeForsen- <semen.neb@gmail.com>
2023-03-02 11:16:19 +0700

    add index file of Players & GameMoves

---
+++ src/durak-game/entity/GameMove/index.ts
@@ -0,0 +1,19 @@
+import { AttackerMove } from "./AttackerMove";
+import { DefenderMove } from "./DefenderMove";
+import { GameMove } from "./GameMove";
+import { InsertAttackCardMove } from "./InsertAttackCardMove";
+import { InsertDefendCardMove } from "./InsertDefendCardMove";
+import { StopAttackMove } from "./StopAttackMove";
+import { StopDefenseMove } from "./StopDefenseMove";
+import { TransferMove } from "./TransferMove.Defender";
+
+export {
+  AttackerMove,
+  DefenderMove,
+  GameMove,
+  InsertAttackCardMove,
+  InsertDefendCardMove,
+  StopAttackMove,
+  StopDefenseMove,
+  TransferMove,
+};
---
+++ src/durak-game/entity/Players/index.ts
@@ -0,0 +1,12 @@
+import Attacker from "./Attacker";
+import Defender from "./Defender";
+import Player from "./Player";
+import Players from "./Players";
+
+export {
+  Attacker,
+  Defender,
+  Player,
+  Players,
+};
+
