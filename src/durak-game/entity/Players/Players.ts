2fc46db2049209555e5fb9d6cbf8b06f396a505d
GeForsen- <semen.neb@gmail.com>
2023-03-02 11:19:17 +0700

    Players: remove defineIndexes & assertPlayer, swap make method params & rename param

--- src/durak-game/entity/Players/Players.ts
+++ src/durak-game/entity/Players/Players.ts
@@ -9,7 +9,6 @@ export default class Players {
   constructor(users: LobbyUsers) {
     this.__value = users.value.map((user) => new Player(user));
     this.defineSidePlayers();
-    this.defineIndexes();
   }
 
   get count() {
@@ -17,8 +16,8 @@ export default class Players {
   }
 
   receiveFirstCards({ talonCards, howMany }: { talonCards: Card[], howMany: number }) {
-    for (let i = 0, playerI = 0; talonCards.length !== 0; i++, playerI = i % this.count) {
-      const player = this.__value[playerI];
+    for (let i = 0, playerIndex = 0; talonCards.length !== 0; i++, playerIndex = i % this.count) {
+      const player = this.__value[playerIndex];
       const startIndex = talonCards.length - howMany;
       const cards = talonCards.splice(startIndex, howMany);
       player.receiveCards(...cards);
@@ -33,7 +32,7 @@ export default class Players {
 
   tryGetAttacker(): Attacker {
     const attacker = this.getAttacker();
-    this.assertPlayer(attacker, "Атакующий не найден");
+    assert.ok(attacker, "Атакующий не найден");
     return attacker;
   }
 
@@ -45,7 +44,7 @@ export default class Players {
 
   tryGetDefender(): Defender {
     const defender = this.getDefender();
-    this.assertPlayer(defender, "Защищающийся не найден");
+    assert.ok(defender, "Защищающийся не найден");
     return defender;
   }
 
@@ -55,7 +54,7 @@ export default class Players {
 
   tryGetPlayer({ accname }: LobbyUserIdentifier): Player {
     const player = this.getPlayer({ accname });
-    this.assertPlayer(player);
+    assert.ok(player, "Игрок не найден");
     return player;
   }
 
@@ -76,11 +75,12 @@ export default class Players {
   }
 
   make<P extends Player>(
-    playerOrIdentifier: Player | LobbyUserIdentifier, PlayerP: { new(player: Player): P },
+    PlayerLike: { new(player: Player): P },
+    playerOrIdentifier: Player | LobbyUserIdentifier,
   ): P {
     const accname = this.getAccname(playerOrIdentifier);
     const playerIndex = this.getPlayerIndex({ accname });
-    const instance = new PlayerP(this.__value[playerIndex]);
+    const instance = new PlayerLike(this.__value[playerIndex]);
     this.__value[playerIndex] = instance;
     return instance;
   }
@@ -106,13 +106,5 @@ export default class Players {
   private getRightPlayerIndex(index: number): number {
     const isFirstPlayer = index === 0;
     return isFirstPlayer ? this.count - 1 : index - 1;
-  }
-
-  private defineIndexes(): void {
-    this.__value.forEach((player, index) => player.index = index);
-  }
-
-  private assertPlayer(player: Player | undefined, message?: string): asserts player is Player {
-    if (player === undefined) throw new Error(message ?? "Игрок не найден");
   }
 }
