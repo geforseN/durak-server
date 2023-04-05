10d62a9e1d44e84988c181dc75d1fa6ec481d6a5
GeForsen- <semen.neb@gmail.com>
2023-04-06 00:25:04 +0700

    PLayers index: add SuperPlayer

--- src/durak-game/entity/Players/index.ts
+++ src/durak-game/entity/Players/index.ts
@@ -2,10 +2,12 @@ import Attacker from "./Attacker";
 import Defender from "./Defender";
 import Player from "./Player";
 import Players from "./Players";
+import SuperPlayer from "./SuperPlayer";
 
 export {
   Attacker,
   Defender,
+  SuperPlayer,
   Player,
   Players,
 };
