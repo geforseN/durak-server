c3974f636875dc6f0a2e646c6d40fdbef6743fef
GeForsen- <semen.neb@gmail.com>
2023-04-05 23:48:05 +0700

    make DeskSlot index file

---
+++ src/durak-game/entity/DeskSlot/index.ts
@@ -0,0 +1,13 @@
+import DeskSlot from "./DeskSlot.abstract";
+import EmptySlot from "./EmptySlot";
+import UnbeatenSlot from "./UnbeatenSlot";
+import UnbeatenTrumpSlot from "./UnbeatenTrumpSlot";
+import DefendedSlot from "./DefendedSlot";
+
+export {
+  DeskSlot,
+  EmptySlot,
+  UnbeatenSlot,
+  UnbeatenTrumpSlot,
+  DefendedSlot,
+};
