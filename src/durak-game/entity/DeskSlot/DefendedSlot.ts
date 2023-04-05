f53688e7ea5b931b01d73a98ac53ba17c639e3cb
GeForsen- <semen.neb@gmail.com>
2023-04-05 23:47:46 +0700

    DefendedSlot: init

---
+++ src/durak-game/entity/DeskSlot/DefendedSlot.ts
@@ -0,0 +1,28 @@
+import Card from "../Card";
+import { DeskSlot } from "./index";
+
+export default class DefendedSlot extends DeskSlot {
+  constructor(public attackCard: Card, public defendCard: Card) {
+    super();
+  }
+
+  get value(): [Card, Card] {
+    return [this.attackCard, this.defendCard];
+  }
+
+  assertAttack({ card: _card }: { card: Card }) {
+    return Promise.reject("Слот полностью занят");
+  }
+
+  assertDefense({ card: _card }: { card: Card }) {
+    return Promise.reject("Карта уже побита");
+  }
+
+  allowsTransfer({ card }: { card: Card }) {
+    const { rank } = card;
+    return new Promise<Card>((resolve, reject) => {
+      if (this.attackCard.hasSame({ rank })) resolve(card);
+      reject("Нельзя перевести: нет схожего ранга");
+    });
+  }
+}
