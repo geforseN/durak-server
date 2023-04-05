5a48650a9a43fc05a46912963e8805b6df3cfb12
GeForsen- <semen.neb@gmail.com>
2023-04-05 23:47:37 +0700

    UnbeatenTrumpSlot: init

---
+++ src/durak-game/entity/DeskSlot/UnbeatenTrumpSlot.ts
@@ -0,0 +1,16 @@
+import Card from "../Card";
+import { UnbeatenSlot } from "./index";
+
+export default class UnbeatenTrumpSlot extends UnbeatenSlot {
+  constructor(public attackCard: Card) {
+    super(attackCard);
+  }
+
+  assertDefense({ card }: { card: Card }) {
+    return new Promise<Card>((resolve, reject) => {
+      if (!card.isTrump) reject("Козырную карту можно побить только козырной");
+      if (this.attackCard.power > card.power) reject("Вы кинули слабую карту");
+      resolve(card);
+    });
+  }
+}
