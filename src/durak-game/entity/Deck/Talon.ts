688039219f678d7ead844be710a8715c7b910046
GeForsen- <semen.neb@gmail.com>
2023-04-06 20:00:02 +0700

    Talon: add injectCardIsTrump private method

--- src/durak-game/entity/Deck/Talon.ts
+++ src/durak-game/entity/Deck/Talon.ts
@@ -45,4 +45,11 @@ export default class Talon extends Deck {
   private swapCards(i: number, k: number) {
     [this._value[k], this._value[i]] = [this._value[i], this._value[k]];
   }
+
+  private injectCardIsTrump() {
+    for (let i = this._value.length - 1; i > 0; i--) {
+      const card = this._value[i];
+      card.isTrump = card.suit === this.trumpSuit;
+    }
+  }
 }
