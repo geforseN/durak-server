09e833714a4666c37570fe5a9dbb7d4c76039657
GeForsen- <semen.neb@gmail.com>
2023-04-23 19:43:54 +0700

    handleStopMove: refactor

--- src/namespaces/games/methods/handle-stop-move.ts
+++ src/namespaces/games/methods/handle-stop-move.ts
@@ -1,14 +1,17 @@
 import DurakGame from "../../../durak-game/DurakGame";
-import { GameSocket } from "../game.service";
 
 export default function handleStopMove(
-  this: { game: DurakGame, accname: string } & GameSocket
+  this: { game: DurakGame, accname: string }
 ) {
-  const { game, accname } = this;
-  const player = game.players.tryGetPlayer({ accname });
-  if (game.round.currentMove.allowedPlayerAccname !== accname) throw new Error("Нет права ходить");
-  if (game.desk.isEmpty) throw new Error("Нельзя закончить раунд с пустым столом");
-  if (game.players.isDefender(player) || game.players.isAttacker(player)) {
+  const { game, accname: id } = this;
+  if (game.round.currentMove.player.id !== id) {
+    throw new Error("Нет права ходить");
+  }
+  if (game.desk.isEmpty) {
+    throw new Error("Нельзя закончить раунд с пустым столом");
+  }
+  const player = game.players.getPlayer({ id });
+  if (game.players.isSuperPlayer(player)) {
     player.stopMove({ game });
   } else throw new Error("Нет права");
 }
