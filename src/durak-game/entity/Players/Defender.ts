f0998c20638664685ceff5252020931574e6fc7d
GeForsen- <semen.neb@gmail.com>
2023-04-23 20:10:38 +0700

    Defender: big changes

--- src/durak-game/entity/Players/Defender.ts
+++ src/durak-game/entity/Players/Defender.ts
@@ -1,5 +1,6 @@
-import DurakGame from "../../DurakGame";
-import { Player, SuperPlayer } from "./index";
+import assert from "node:assert";
+import DurakGame, { CanReceiveCards } from "../../DurakGame";
+import { Player } from "./index";
 import {
   AttackerMove,
   DefenderGaveUpMove,
@@ -9,10 +10,19 @@ import {
   TransferMove,
 } from "../GameMove";
 import Card from "../Card";
+import SuperPlayer from "./SuperPlayer";
+import GameDefenderService from "../Services/Defender.service";
 
-export default class Defender extends SuperPlayer {
+export default class Defender extends SuperPlayer implements CanReceiveCards {
+  service?: GameDefenderService;
+
   constructor(player: Player) {
     super(player);
+  }
+
+  override receiveCards(...cards: Card[]) {
+    super.receiveCards(...cards);
+    this.service?.receiveCards({ defender: this, cards });
   }
 
   canDefend(cardCount: number) {
@@ -21,46 +31,53 @@ export default class Defender extends SuperPlayer {
 
   async putCardOnDesk({ game, index, card }: { game: DurakGame, card: Card, index: number }): Promise<void | never> {
     const slot = game.desk.getSlot({ index });
-    await slot.assertDefense({ card });
+    await slot.assertCanBeDefended({ card });
     this.putDefendCard({ game, card, slotIndex: index });
     this.handleAfterCardPut({ game });
   }
 
   makeTransferMove({ game, index, card }: { game: DurakGame, card: Card, index: number }): void {
     this.putTransferCard({ game, card, slotIndex: index });
-    game.players.manager.makeNewDefender(this.right);
-    const player = game.players.manager.makeNewAttacker(this);
-    game.round.pushNextMove(AttackerMove, { player });
+    game.players.manager.makeNewAttacker(this);
+    const newDefender = game.players.manager.makeDefender(this.left);
+    return game.round.pushNextMove(DefenderMove, { player: newDefender });
   }
 
   stopMove({ game }: { game: DurakGame }): void {
     if (!game.desk.isDefended) {
       game.round.updateCurrentMoveTo(DefenderGaveUpMove, { player: this });
-      return this.letPrimalAttackerMove({ game }); // PUT MORE CARDS IN PURSUIT (VDOGONKU)
+      // here defender can not defend more
+      // if desk is full THAN go to next round
+      // OR THAN let put more cards in pursuit (vdogonku)
+      return game.desk.allowsMoves
+        ? this.letPrimalAttackerMove({ game })
+        : game.handleLostDefence(this);
     }
+    // desk is defended
+    // let primal attacker try make defender loser
     game.round.updateCurrentMoveTo(StopDefenseMove, { player: this });
-    if (game.desk.allowsMoves) {
-      return this.letPrimalAttackerMove({ game });
-    }
-    return game.handleWonDefence(this);
+    return this.letPrimalAttackerMove({ game });
   }
 
   private putDefendCard({ game, card, slotIndex }: { game: DurakGame, card: Card, slotIndex: number }) {
-    this.putCard({game, card, slotIndex}, InsertDefendCardMove);
+    this.putCard({ game, card, slotIndex }, InsertDefendCardMove);
   }
 
   private handleAfterCardPut({ game }: { game: DurakGame }): void {
-    if (!game.desk.isDefended) {
-      return game.round.pushNextMove(DefenderMove, { player: this });
+    if (game.desk.isDefended
+      && (!this.hand.count || !game.desk.allowsMoves)
+    ) {
+      assert.ok(game.desk.isDefended, "У защитника нет карт, а стол незащищён. Так нельзя.");
+      return game.handleWonDefence(this);
     }
-    if (game.desk.allowsMoves && this.hand.count) {
+    if (game.desk.isDefended && game.desk.allowsMoves) {
       this.letPrimalAttackerMove({ game });
     }
-    return game.handleWonDefence(this);
+    return this.makeAnotherDefenseMove({ game });
   }
 
   private putTransferCard({ game, card, slotIndex }: { game: DurakGame, card: Card, slotIndex: number }) {
-    this.putCard({game, card, slotIndex}, TransferMove);
+    this.putCard({ game, card, slotIndex }, TransferMove);
   }
 
   private letPrimalAttackerMove({ game }: { game: DurakGame }) {
@@ -70,12 +87,20 @@ export default class Defender extends SuperPlayer {
 
   private putCard<M extends TransferMove | InsertDefendCardMove>(
     { game, card, slotIndex }: { game: DurakGame, card: Card, slotIndex: number },
-    Move: { new(arg: any): M }
+    Move: { new(arg: any): M },
   ) {
     this.remove({ card });
     const moveContext = { player: this, card, slotIndex };
     // @ts-ignore
     game.round.updateCurrentMoveTo(Move, moveContext);
-    game.desk.insertCard({ card, index: slotIndex });
+    game.desk.receiveCard({ card, index: slotIndex, who: this });
+  }
+
+  private makeAnotherDefenseMove({ game }: { game: DurakGame }) {
+    game.round.pushNextMove(DefenderMove, { player: this });
+  }
+
+  injectService(defenderService: GameDefenderService) {
+    this.service = defenderService;
   }
 }
