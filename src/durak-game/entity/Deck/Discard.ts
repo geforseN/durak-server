b3507f693445e4bf02ee81074e0b92669b5cd68c
GeForsen- <semen.neb@gmail.com>
2023-04-23 20:03:33 +0700

    Discard: implement receiveCards, add service support

--- src/durak-game/entity/Deck/Discard.ts
+++ src/durak-game/entity/Deck/Discard.ts
@@ -1,12 +1,21 @@
 import Deck from "./Deck.abstract";
 import Card from "../Card";
+import { CanReceiveCards } from "../../DurakGame";
+import GameDiscardService from "../Services/Discard.service";
 
-export default class Discard extends Deck {
+export default class Discard extends Deck implements CanReceiveCards {
+  service?: GameDiscardService;
+
   constructor() {
     super();
   }
 
-  push(...cards: Card[]): void {
+  receiveCards(...cards: Card[]): void {
     this._value.push(...cards);
+    this.service?.receiveCards(cards);
+  }
+
+  injectService(gameDeskService: GameDiscardService) {
+    this.service = gameDeskService;
   }
 }
