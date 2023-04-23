8efd96f85a0c8febee9706da27f4dfd38bc8dfd1
GeForsen- <semen.neb@gmail.com>
2023-04-23 19:46:13 +0700

    GameStateDTO: refactor & lint

--- src/durak-game/DTO/GameState.ts
+++ src/durak-game/DTO/GameState.ts
@@ -1,22 +1,21 @@
+import DurakGame from "../DurakGame";
 import Card from "../entity/Card";
 import Self from "./Self.dto";
 import Enemy from "./Enemy.dto";
-import DeskSlot from "../entity/DeskSlot";
-import DurakGame from "../DurakGame";
-import { LobbyUserIdentifier } from "../../namespaces/lobbies/entity/lobby-users";
+import { DeskSlot } from "../entity/DeskSlot";
 
 export default class GameState {
   self: Self;
   enemies: Enemy[];
   deskSlots: DeskSlot[];
   trumpCard: Card;
-  allowedPlayerAccname: string
+  allowedPlayerAccname: string;
 
-  constructor(game: DurakGame, accname: LobbyUserIdentifier["accname"]) {
-    this.self = new Self(game.players.tryGetPlayer({ accname }));
-    this.enemies = game.players.getPlayerEnemies({ accname }).map(((enemy) => new Enemy(enemy)));
+  constructor(game: DurakGame, id: string) {
+    this.self = new Self(game.players.getPlayer({ id }));
+    this.enemies = game.players.manager.getPlayerEnemies({ id }).map(((enemy) => new Enemy(enemy)));
     this.deskSlots = game.desk.slots;
     this.trumpCard = game.talon.trumpCard;
-    this.allowedPlayerAccname = game.round.currentMove.allowedPlayerAccname;
+    this.allowedPlayerAccname = game.round.currentMove.player.id;
   }
 }
