3df00de81dcb9efdf9ecac471e2c6366a99fb502
GeForsen- <semen.neb@gmail.com>
2023-04-23 19:31:47 +0700

    DeskService: remove unused

--- src/durak-game/entity/Services/Desk.service.ts
+++ src/durak-game/entity/Services/Desk.service.ts
@@ -1,7 +1,6 @@
 import Card from "../Card";
 import { GamesIO } from "../../../namespaces/games/games.types";
-import { CanReceiveCards } from "../../DurakGame";
-import { Defender, SuperPlayer } from "../Players";
+import { SuperPlayer } from "../Players";
 
 export default class GameDeskService {
   constructor(private namespace: GamesIO.NamespaceIO) {
@@ -9,22 +8,6 @@ export default class GameDeskService {
 
   clearDesk() {
     this.namespace.emit("desk__clear");
-  }
-
-  provideCards({ target, cards }: { target: CanReceiveCards, cards: Card[] }) {
-    if (target instanceof Defender) {
-      return this.provideToDefender({ defender: target, cards });
-    }
-    throw new Error("Can provide cards to " + target);
-  }
-
-  private provideToDiscard({ cards }: { cards: Card[] }) {
-    this.namespace.emit("discard__receiveCards", cards.length);
-  }
-
-  private provideToDefender({ defender, cards }: { defender: Defender, cards: Card[] }) {
-    this.namespace.to(defender.id).emit("player__receiveCards", cards);
-    this.namespace.except(defender.id).emit("enemy__changeCardCount", defender.id, defender.hand.count);
   }
 
   insertCard({ card, index, who }: { card: Card, index: number, who: SuperPlayer }) {
