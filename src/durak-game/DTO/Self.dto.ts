7a0602d4427a4b0f4e48b4c968d7003b140ced24
GeForsen- <semen.neb@gmail.com>
2023-04-23 23:06:40 +0700

    SelfDTO: rename class & add CardDTO usage

--- src/durak-game/DTO/Self.dto.ts
+++ src/durak-game/DTO/Self.dto.ts
@@ -1,14 +1,14 @@
 import Player from "../entity/Players/Player";
 import { LobbyUser } from "../../namespaces/lobbies/entity/lobby-users";
-import Card from "../entity/Card";
+import CardDTO from "./Card.dto";
 
-export default class Self {
-  cards: Card[];
+export default class SelfDTO {
+  cards: CardDTO[];
   info: LobbyUser;
   role: string;
 
   constructor(cardPlayer: Player) {
-    this.cards = cardPlayer.hand.value;
+    this.cards = cardPlayer.hand.value.map((card) => new CardDTO(card));
     this.info = cardPlayer.info;
     this.role = cardPlayer.constructor.name;
   }
