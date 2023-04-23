9e714f827a5e17566c3df0665a03148e12038960
GeForsen- <semen.neb@gmail.com>
2023-04-23 19:36:08 +0700

    PLayers index: replace lines

--- src/durak-game/entity/Players/index.ts
+++ src/durak-game/entity/Players/index.ts
@@ -1,14 +1,14 @@
+import Player from "./Player";
+import SuperPlayer from "./SuperPlayer";
 import Attacker from "./Attacker";
 import Defender from "./Defender";
-import Player from "./Player";
 import Players from "./Players";
-import SuperPlayer from "./SuperPlayer";
 
 export {
+  Player,
+  SuperPlayer,
   Attacker,
   Defender,
-  SuperPlayer,
-  Player,
   Players,
 };
 
