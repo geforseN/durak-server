7d7515dba69f722767450e3d31307452190c74d3
GeForsen- <semen.neb@gmail.com>
2023-04-23 23:22:09 +0700

    GameState: change property name & update imports

--- src/durak-game/DTO/GameState.ts
+++ src/durak-game/DTO/GameState.ts
@@ -1,22 +1,20 @@
 import DurakGame from "../DurakGame";
-import Card from "../entity/Card";
-import Self from "./Self.dto";
-import Enemy from "./Enemy.dto";
-import { DeskSlot } from "../entity/DeskSlot";
+import SelfDTO from "./Self.dto";
+import EnemyDTO from "./Enemy.dto";
 
 export default class GameState {
-  self: Self;
-  enemies: Enemy[];
-  deskSlots: DeskSlot[];
-  trumpCard: Card;
-  allowedPlayerAccname: string;
+  self: SelfDTO;
+  enemies: EnemyDTO[];
+  deskSlots: DurakGame["desk"]["slots"]
+  trumpCard: DurakGame["talon"]["trumpCard"];
+  allowedPlayerId: string;
 
   constructor(game: DurakGame, playerId: string) {
     const id = playerId;
-    this.self = new Self(game.players.getPlayer({ id }));
-    this.enemies = game.players.manager.getPlayerEnemies({ id }).map(((enemy) => new Enemy(enemy)));
+    this.self = new SelfDTO(game.players.getPlayer({ id }));
+    this.enemies = game.players.manager.getPlayerEnemies({ id }).map(((enemy) => new EnemyDTO(enemy)));
     this.deskSlots = game.desk.slots;
     this.trumpCard = game.talon.trumpCard;
-    this.allowedPlayerAccname = game.round.currentMove.player.id;
+    this.allowedPlayerId = game.round.currentMove.player.id;
   }
 }
