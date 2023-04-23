c034b66f13e13bb89a033c75e7e75582282364f7
GeForsen- <semen.neb@gmail.com>
2023-04-23 23:19:05 +0700

    EnemyDTO: rename class & change role type

--- src/durak-game/DTO/Enemy.dto.ts
+++ src/durak-game/DTO/Enemy.dto.ts
@@ -1,14 +1,15 @@
 import Player from "../entity/Players/Player";
 import { LobbyUser } from "../../namespaces/lobbies/entity/lobby-users";
+import { PlayerRole } from "../../namespaces/games/games.types";
 
-export default class Enemy {
+export default class EnemyDTO {
   cardCount: number;
   info: LobbyUser;
-  role: string;
+  role: PlayerRole;
 
   constructor(cardPlayer: Player) {
     this.cardCount = cardPlayer.hand.count;
     this.info = cardPlayer.info;
-    this.role = cardPlayer.constructor.name;
+    this.role = cardPlayer.constructor.name as PlayerRole;
   }
 }
