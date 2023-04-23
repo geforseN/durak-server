109d917bbc6a071863873ca1203fd8394f876272
GeForsen- <semen.neb@gmail.com>
2023-04-23 23:17:51 +0700

    SelfDTO: change role type to PlayerRole

--- src/durak-game/DTO/Self.dto.ts
+++ src/durak-game/DTO/Self.dto.ts
@@ -1,15 +1,16 @@
 import Player from "../entity/Players/Player";
 import { LobbyUser } from "../../namespaces/lobbies/entity/lobby-users";
 import CardDTO from "./Card.dto";
+import { PlayerRole } from "../../namespaces/games/games.types";
 
 export default class SelfDTO {
   cards: CardDTO[];
   info: LobbyUser;
-  role: string;
+  role: PlayerRole;
 
   constructor(cardPlayer: Player) {
     this.cards = cardPlayer.hand.value.map((card) => new CardDTO(card));
     this.info = cardPlayer.info;
-    this.role = cardPlayer.constructor.name;
+    this.role = cardPlayer.constructor.name as PlayerRole;
   }
 }
