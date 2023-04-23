aa7e30ea793fd18611db45c499c7c8d3c34717b3
GeForsen- <semen.neb@gmail.com>
2023-04-23 20:02:34 +0700

    GameService: remove unused &  refactor

--- src/namespaces/games/game.service.ts
+++ src/namespaces/games/game.service.ts
@@ -1,10 +1,7 @@
-import { GamesIO, PlayerRole, UIStatus } from "./games.types";
-import { LobbyUserIdentifier } from "../lobbies/entity/lobby-users";
+import { GamesIO } from "./games.types";
 import Card from "../../durak-game/entity/Card";
 import NotificationAlert from "../../module/notification-alert";
 import Player from "../../durak-game/entity/Players/Player";
-import Defender from "../../durak-game/entity/Players/Defender";
-import Attacker from "../../durak-game/entity/Players/Attacker";
 import GameState from "../../durak-game/DTO/GameState";
 import DurakGame from "../../durak-game/DurakGame";
 
@@ -14,94 +11,33 @@ export class GameService {
   constructor(private namespace: GamesIO.NamespaceIO) {
   }
 
-  insertAttackCard({ card, index, socket }: { card: Card, index: number } & GameSocket): this {
-    socket.broadcast.emit("desk__insertAttackCard", card, index);
-    return this;
+  /* @TODO replace me in SuperPlayerService */
+  removeCard({ player, card }: { player: Player, card: Card }) {
+    this.namespace.to(player.id).emit("self__removeCard", card);
+    this.namespace.except(player.id).emit("enemy__changeCardCount", player.id, player.hand.count);
   }
 
-  insertDefendCard({ card, index, socket }: { card: Card, index: number } & GameSocket): this {
-    socket.broadcast.emit("desk__insertDefendCard", card, index);
-    return this;
+  lostRound({ game }: { game: DurakGame }) {
+    const { players: { defender }, round } = game;
+    this.namespace.emit("defender__lostRound", defender.id, round.number);
   }
 
-  changeCardCount({ accname, cardCount, socket }: { accname: string, cardCount: number } & GameSocket): this {
-    socket.except(accname).emit("enemy__changeCardCount", accname, cardCount);
-    return this;
+  wonRound({ game }: { game: DurakGame }) {
+    const { players: { defender }, round } = game;
+    this.namespace.emit("defender__wonRound", defender.id, round.number);
   }
 
-  removeCard({ accname, card, socket }: { accname: string, card: Card } & GameSocket): this {
-    socket.to(accname).emit("self__removeCard", card);
-    return this;
-  }
-
-  setSuperPlayerUI(status: UIStatus, allowedPlayer: Defender | Attacker): this {
-    const eventName = allowedPlayer instanceof Defender
-      ? "defendUI__setStatus"
-      : "attackUI__setStatus";
-    this.namespace.to(allowedPlayer.info.accname).emit(eventName, status);
-    return this;
-  }
-
-  setDefendUI(status: UIStatus, defender: Defender): this {
-    this.namespace.to(defender.info.accname).emit("defendUI__setStatus", status);
-    return this;
-  }
-
-  setAttackUI(status: UIStatus, attacker: Attacker): this {
-    this.namespace.to(attacker.info.accname).emit("attackUI__setStatus", status);
-    return this;
-  }
-
-  handleError({ accname, error }: LobbyUserIdentifier & { error: unknown }): this {
+  handleError({ accname, error }: { error: unknown, accname: string }) {
     console.log("ERROR: ", error);
     const notification = new NotificationAlert().fromError(error as Error);
     this.namespace.to(accname).emit("notification__send", notification);
-    return this;
   }
 
-  clearDesk(): this {
-    this.namespace.emit("desk__clear");
-    return this;
+  restoreState({ socket, game, playerId }: { game: DurakGame, playerId: string } & GameSocket) {
+    socket.emit("state__restore", new GameState(game, playerId));
   }
 
-  pushFromTalon({ player, cards }: { player: Player, cards: Card[] }): this {
-    this.namespace.to(player.info.accname).emit("player__receiveCards", cards);
-    this.namespace.except(player.info.accname).emit("talon__distributeCards", player.info.accname, cards.length);
-    this.namespace.except(player.info.accname).emit("enemy__changeCardCount", player.info.accname, player.hand.count);
-    return this;
-  }
-
-  pushToDiscard(): this {
-    this.namespace.emit("desk__pushToDiscard");
-    return this;
-  }
-
-  lostRound({ defender }: { defender: Defender }): this {
-    this.namespace.emit("defender__lostRound", defender.info.accname);
-    return this;
-  }
-
-  wonRound({ defender }: { defender: Defender }): this {
-    this.namespace.emit("defender__wonRound", defender.info.accname);
-    return this;
-  }
-
-  moveTrumpCard({ receiver }: { receiver: Player }): this {
-    this.namespace.emit("talon__moveTrumpCardTo", receiver.info.accname);
-    return this;
-  }
-
-  changeRoleTo(role: PlayerRole, player: Player): this {
-    this.namespace.emit("player__changeRole", role, player.info.accname);
-    return this;
-  }
-
-  restoreState({ socket, game, accname }: { game: DurakGame } & LobbyUserIdentifier & GameSocket): this {
-    socket.emit("state__restore", new GameState(game, accname));
-    return this;
-  }
-
-  letMoveTo(accname: string) {
-    this.namespace.emit("player__allowedToMove", accname);
+  end(game: DurakGame) {
+    this.namespace.emit("game__over")
   }
 }
