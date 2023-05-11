0a5d4ab66679551e907c3d9cd753499ff0f1725b
GeForsen- <semen.neb@gmail.com>
2023-05-11 18:41:42 +0700

    DurakGameStateDTO: lint & add many props
    added many props for better frontend representation

--- src/durak-game/DTO/DurakGameState.dto.ts
+++ src/durak-game/DTO/DurakGameState.dto.ts
@@ -5,16 +5,38 @@ import EnemyDTO from "./Enemy.dto";
 export default class DurakGameStateDto {
   self: SelfDTO;
   enemies: EnemyDTO[];
-  deskSlots: DurakGame["desk"]["slots"]
+  deskSlots: DurakGame["desk"]["slots"];
   trumpCard: DurakGame["talon"]["trumpCard"];
   allowedPlayerId: string;
+  isDiscardEmpty: boolean;
+  isTalonEmpty: boolean;
+  isTalonHasOneCard: boolean;
+  isDefenderGaveUp: boolean;
+  playersCount: number;
+  roundNumber: number;
 
   constructor(game: DurakGame, playerId: string) {
-    const id = playerId;
-    this.self = new SelfDTO(game.players.getPlayer({ id }));
-    this.enemies = game.players.manager.getPlayerEnemies({ id }).map(((enemy) => new EnemyDTO(enemy)));
+    const currentPlayer = game.players.getPlayer({ id: playerId });
+    this.self = new SelfDTO(currentPlayer);
+    this.enemies = this.getOrderedEnemies(currentPlayer);
     this.deskSlots = game.desk.slots;
     this.trumpCard = game.talon.trumpCard;
     this.allowedPlayerId = game.round.currentMove.player.id;
+    this.isDiscardEmpty = game.discard.count === 0;
+    this.isTalonEmpty = game.talon.isEmpty;
+    this.isTalonHasOneCard = game.talon.count === 1;
+    this.playersCount = game.settings.maxUserCount;
+    this.roundNumber = game.round.number;
+    this.isDefenderGaveUp = game.round.isDefenderGaveUp;
+  }
+
+  private getOrderedEnemies(player: ReturnType<DurakGame["players"]["getPlayer"]>) {
+    const enemies: EnemyDTO[] = [];
+    let enemy = player.left;
+    while (enemy.id !== player.id) {
+      enemies.push(new EnemyDTO(enemy));
+      enemy = enemy.left;
+    }
+    return enemies;
   }
 }
