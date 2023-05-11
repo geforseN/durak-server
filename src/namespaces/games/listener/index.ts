c96aa65850d041f7683b46e3e0a4e0e6e7f393e1
GeForsen- <semen.neb@gmail.com>
2023-05-11 18:36:32 +0700

    games: add event listeners

---
+++ src/namespaces/games/listener/index.ts
@@ -0,0 +1,28 @@
+import DurakGame from "../../../durak-game/DurakGame";
+import Card from "../../../durak-game/entity/Card";
+import handlePutCardOnDesk from "../methods/handle-put-card-on-desk";
+import handleStopMove from "../methods/handle-stop-move";
+
+export function stopMoveListener(this: { game: DurakGame, playerId: string }) {
+  try {
+    handleStopMove.call(this);
+  } catch (error) {
+    console.trace(error);
+    const { game, playerId } = this;
+    game.service?.handleError({ playerId, error });
+  }
+}
+
+export async function cardPlaceListener(
+  this: { game: DurakGame, playerId: string },
+  card: Pick<Card, "rank" | "suit">,
+  slotIndex: number,
+) {
+  try {
+    await handlePutCardOnDesk.call(this, card, slotIndex);
+  } catch (error) {
+    console.trace(error);
+    const { game, playerId } = this;
+    game.service?.handleError({ playerId, error });
+  }
+}
