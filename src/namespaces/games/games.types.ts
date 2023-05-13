ed65aee7a76b74ab6d9bfb9a53b51562a2080c57
GeForsen- <semen.neb@gmail.com>
2023-05-13 13:44:01 +0700

    games.types: add & remove event types & change order & lint

--- src/namespaces/games/games.types.ts
+++ src/namespaces/games/games.types.ts
@@ -1,42 +1,39 @@
 import { Namespace, Socket } from "socket.io";
 import Card from "../../durak-game/entity/Card";
-import { Suit } from "../../durak-game/utility";
 import NotificationAlert from "../../module/notification-alert";
 import DurakGameStateDto from "../../durak-game/DTO/DurakGameState.dto";
 
 export type PlayerRole = "Defender" | "Attacker" | "Player";
-export type UIStatus = "revealed" | "hidden" | "freeze";
 
 export namespace GamesIO {
   export type ClientToServerEvents = {
-    "state__restore": () => void
-    "superPlayer__stopMove": () => void
-    "superPlayer__putCardOnDesk": (card: Card, slotIndex: number) => void
+    "player__gaveUp": () => void;
+    "superPlayer__stopMove": () => void;
+    "superPlayer__putCardOnDesk": (card: Card, slotIndex: number) => void;
   }
 
   export type ServerToClientEvents = {
     "state__restore": (state: DurakGameStateDto) => void;
     "notification__send": (notification: NotificationAlert) => void;
+    "game__over": () => void
 
-    "talon__distributeCards": (id: string, cardCount: number) => void;
-    "talon__showTrumpCard": (trumpCard: Card) => void;
-    "talon__moveTrumpCardTo": (id: string) => void;
-
-    "defendUI__setStatus": (status: UIStatus) => void;
-    "attackUI__setStatus": (status: UIStatus) => void;
+    "talon__distributeCardsTo": (playerId: string, cardCount: number) => void;
+    "talon__keepOnlyTrumpCard": () => void;
+    "talon__moveTrumpCardTo": (playerId: string) => void;
 
     "player__changeRole": (role: PlayerRole, id: string) => void;
     "player__receiveCards": (cards: Card[]) => void;
     "player__allowedToMove": (id: string) => void;
     "self__removeCard": (card: Card) => void;
     "enemy__changeCardCount": (id: string, cardCount: number) => void;
+    "defender__gaveUp": () => void;
     "defender__lostRound": (id: string, roundNumber: number) => void;
     "defender__wonRound": (id: string, roundNumber: number) => void;
 
     "desk__clear": () => void;
     "player__insertCard": (card: Card, slotIndex: number, whoId: string) => void;
     "discard__receiveCards": (cardCount: number) => void;
-    "game__over": () => void
+    "discard__setIsNotEmpty": () => void;
   }
 
   export type InterServerEvents = {}
