219d104560a2e9dd43d4f20020634df774d6441a
GeForsen- <semen.neb@gmail.com>
2023-05-15 18:46:31 +0700

    DefenderMove: add defaultBehaviour logic

--- src/durak-game/entity/GameMove/DefenderMove.ts
+++ src/durak-game/entity/GameMove/DefenderMove.ts
@@ -3,13 +3,8 @@ import { GameMove } from "./GameMove";
 
 export class DefenderMove extends GameMove<Defender> {
   get defaultBehaviour(): NodeJS.Timeout {
-    // тут нужно запрограммировать что-то одно
-    // - сразу засчитать защитника проигравшим
-    // - самим найти нужную карту, которую нужно отбить
-    // NOTE: важно не засчитать защитника проигравшим,
-    //       IF стол защищён (иначе защитнику будет обидно :-( )
     return setTimeout(() => {
-
+      this.player.stopMove({ game: this.game });
     });
   }
 }
