381aaa72da6b5db5f5866df7320df2b2e11ad157
GeForsen- <semen.neb@gmail.com>
2023-05-15 18:14:50 +0700

    games.types: change card related types & add param player__allowedToMove

--- src/namespaces/games/games.types.ts
+++ src/namespaces/games/games.types.ts
@@ -1,7 +1,7 @@
 import { Namespace, Socket } from "socket.io";
-import Card from "../../durak-game/entity/Card";
 import NotificationAlert from "../../module/notification-alert";
 import DurakGameStateDto from "../../durak-game/DTO/DurakGameState.dto";
+import CardDTO from "../../durak-game/DTO/Card.dto";
 
 export type PlayerRole = "Defender" | "Attacker" | "Player";
 
@@ -9,7 +9,7 @@ export namespace GamesIO {
   export type ClientToServerEvents = {
     "player__gaveUp": () => void;
     "superPlayer__stopMove": () => void;
-    "superPlayer__putCardOnDesk": (card: Card, slotIndex: number) => void;
+    "superPlayer__putCardOnDesk": (card: CardDTO, slotIndex: number) => void;
   }
 
   export type ServerToClientEvents = {
@@ -22,16 +22,16 @@ export namespace GamesIO {
     "talon__moveTrumpCardTo": (playerId: string) => void;
 
     "player__changeRole": (role: PlayerRole, id: string) => void;
-    "player__receiveCards": (cards: Card[]) => void;
-    "player__allowedToMove": (id: string) => void;
-    "self__removeCard": (card: Card) => void;
+    "player__receiveCards": (cards: CardDTO[]) => void;
+    "player__allowedToMove": (id: string, timeEnd: number) => void;
+    "self__removeCard": (card: CardDTO) => void;
     "enemy__changeCardCount": (id: string, cardCount: number) => void;
     "defender__gaveUp": () => void;
     "defender__lostRound": (id: string, roundNumber: number) => void;
     "defender__wonRound": (id: string, roundNumber: number) => void;
 
     "desk__clear": () => void;
-    "player__insertCard": (card: Card, slotIndex: number, whoId: string) => void;
+    "player__insertCard": (card: CardDTO, slotIndex: number, whoId: string) => void;
     "discard__receiveCards": (cardCount: number) => void;
     "discard__setIsNotEmpty": () => void;
   }
