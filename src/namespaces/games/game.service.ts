ee6599ab7ece095cf0d8cfcddcac7b1eae578c55
GeForsen- <semen.neb@gmail.com>
2023-05-15 17:57:15 +0700

    GameService.removeCard: delete

--- src/namespaces/games/game.service.ts
+++ src/namespaces/games/game.service.ts
@@ -1,7 +1,5 @@
 import { GamesIO } from "./games.types";
-import Card from "../../durak-game/entity/Card";
 import NotificationAlert from "../../module/notification-alert";
-import Player from "../../durak-game/entity/Players/Player";
 import DurakGameStateDto from "../../durak-game/DTO/DurakGameState.dto";
 import DurakGame from "../../durak-game/DurakGame";
 
@@ -9,12 +7,6 @@ export type GameSocket = { socket: GamesIO.SocketIO };
 
 export class GameService {
   constructor(private namespace: GamesIO.NamespaceIO) {
-  }
-
-  /* @TODO replace me in SuperPlayerService */
-  removeCard({ player, card }: { player: Player, card: Card }) {
-    this.namespace.to(player.id).emit("self__removeCard", card);
-    this.namespace.except(player.id).emit("enemy__changeCardCount", player.id, player.hand.count);
   }
 
   lostRound({ game }: { game: DurakGame }) {
