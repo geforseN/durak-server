6268878869e92ef7466c33430cbceaa1183e555a
GeForsen- <semen.neb@gmail.com>
2023-05-15 18:06:12 +0700

    GameService.firstDefenderMove: add assert

--- src/durak-game/entity/GameRound.ts
+++ src/durak-game/entity/GameRound.ts
@@ -80,6 +80,7 @@ export default class GameRound {
   get firstDefenderMove(): DefenderMove | never {
     const defenderMove = this.moves.find((move) => move instanceof InsertDefendCardMove || move instanceof DefenderGaveUpMove);
     assert.ok(defenderMove, "Нет защищающегося хода");
+    assert.ok(defenderMove instanceof DefenderMove, "Ход не является защищающимся");
     return defenderMove;
   }
 
