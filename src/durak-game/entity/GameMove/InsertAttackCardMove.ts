8e3635cddaca8a8a126d823bc54ef9502dd35215
GeForsen- <semen.neb@gmail.com>
2023-05-15 17:50:09 +0700

    InsertAttackCardMove: remove properties & simplify constructor

--- src/durak-game/entity/GameMove/InsertAttackCardMove.ts
+++ src/durak-game/entity/GameMove/InsertAttackCardMove.ts
@@ -1,15 +1,4 @@
-import { AttackerMove, AttackerMoveConstructorArgs } from "./AttackerMove";
-import Card from "../Card";
-
-type InsertAttackCardMoveConstructorArgs = { card: Card, slotIndex: number } & AttackerMoveConstructorArgs;
+import { AttackerMove } from "./AttackerMove";
 
 export class InsertAttackCardMove extends AttackerMove {
-  card: Card;
-  slotIndex: number;
-
-  constructor({ player, deskCardCount, ...args }: InsertAttackCardMoveConstructorArgs) {
-    super({ player, deskCardCount });
-    this.slotIndex = args.slotIndex;
-    this.card = args.card;
-  }
 }
