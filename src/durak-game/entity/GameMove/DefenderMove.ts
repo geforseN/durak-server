61f03fed0b69b50f1eb1f81ebdfdb0c371ad518a
GeForsen- <semen.neb@gmail.com>
2023-05-15 17:49:10 +0700

    DefenderMove: simplify constructor & add get defaultBehaviour stub

--- src/durak-game/entity/GameMove/DefenderMove.ts
+++ src/durak-game/entity/GameMove/DefenderMove.ts
@@ -1,10 +1,15 @@
 import Defender from "../Players/Defender";
-import { GameMove, GameMoveConstructorArgs } from "./GameMove";
+import { GameMove } from "./GameMove";
 
-export type DefenderMoveConstructorArgs = GameMoveConstructorArgs<Defender>;
+export class DefenderMove extends GameMove<Defender> {
+  get defaultBehaviour(): NodeJS.Timeout {
+    // тут нужно запрограммировать что-то одно
+    // - сразу засчитать защитника проигравшим
+    // - самим найти нужную карту, которую нужно отбить
+    // NOTE: важно не засчитать защитника проигравшим,
+    //       IF стол защищён (иначе защитнику будет обидно :-( )
+    return setTimeout(() => {
 
-export class DefenderMove extends GameMove {
-  constructor({ player, deskCardCount }: DefenderMoveConstructorArgs) {
-    super({ player, deskCardCount });
+    });
   }
 }
