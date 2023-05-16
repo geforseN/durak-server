5620839bd75215402145511a11a85035581f08aa
GeForsen- <semen.neb@gmail.com>
2023-05-16 17:06:00 +0700

    GameMove: add abstract putCardOnDesk & make protected constructor & lint

--- src/durak-game/entity/GameMove/GameMove.ts
+++ src/durak-game/entity/GameMove/GameMove.ts
@@ -1,13 +1,16 @@
 import Player from "../Players/Player";
 import DurakGame from "../../DurakGame";
+import Card from "../Card";
 
 export abstract class GameMove<P extends Player> {
   player: P;
   game: DurakGame;
-  abstract defaultBehaviour: NodeJS.Timeout
+  abstract defaultBehaviour: NodeJS.Timeout;
 
-  constructor({ player, game }: { player: P, game: DurakGame }) {
+  protected constructor({ player, game }: { player: P, game: DurakGame }) {
     this.player = player;
     this.game = game;
   }
+
+  abstract putCardOnDesk(card: Card, index: number): Promise<void>
 }
