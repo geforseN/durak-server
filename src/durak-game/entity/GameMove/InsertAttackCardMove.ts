c89a2a2b8da526fe4ff31fbe4e8c885c4da0d890
GeForsen- <semen.neb@gmail.com>
2023-05-16 17:31:32 +0700

    InsertAttackCardMove: add logic

--- src/durak-game/entity/GameMove/InsertAttackCardMove.ts
+++ src/durak-game/entity/GameMove/InsertAttackCardMove.ts
@@ -1,4 +1,36 @@
 import { AttackerMove } from "./AttackerMove";
+import Card from "../Card";
+
+type ConstructorArg =
+  ConstructorParameters<typeof AttackerMove>[number]
+  & { card: Card, slotIndex: number };
 
 export class InsertAttackCardMove extends AttackerMove {
+  card: Card;
+  slotIndex: number;
+
+  constructor(arg: ConstructorArg) {
+    super(arg);
+    this.card = new Card(arg.card);
+    this.slotIndex = arg.slotIndex;
+    this.#insertCard();
+  }
+
+  #insertCard() {
+    return this.game.desk.receiveCard({
+      card: this.card,
+      index: this.slotIndex,
+      who: this.player,
+    });
+  }
+
+  handleAfterCardInsert() {
+    if (this.player.hand.isEmpty
+      || !this.game.players.defender.canDefend(this.game.desk.unbeatenCardCount)
+      || !this.game.desk.allowsAttackerMove
+    ) {
+      return this.game.round.giveDefenderLastChance();
+    }
+    return this.game.round.giveCurrentAttackerMove();
+  }
 }
