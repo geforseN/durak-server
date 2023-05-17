0b61a004adacd3c23bcfa0678a8ac219236d93ec
GeForsen- <semen.neb@gmail.com>
2023-05-17 15:20:48 +0700

    Defender: remove stopMove & private related methods & add canWinDefense

--- src/durak-game/entity/Players/Defender.ts
+++ src/durak-game/entity/Players/Defender.ts
@@ -1,13 +1,4 @@
 import DurakGame, { CanReceiveCards } from "../../DurakGame";
-import {
-  AttackerMove,
-  DefenderGaveUpMove,
-  DefenderMove,
-  InsertDefendCardMove,
-  StopDefenseMove,
-  TransferMove,
-} from "../GameMove";
-import Card from "../Card";
 import SuperPlayer from "./SuperPlayer";
 
 export default class Defender extends SuperPlayer implements CanReceiveCards {
@@ -15,44 +6,18 @@ export default class Defender extends SuperPlayer implements CanReceiveCards {
     return this.canTakeMore({ cardCount });
   }
 
-  makeTransferMove({ game, index, card }: { game: DurakGame, card: Card, index: number }): void {
-    this.putTransferCard({ game, card, slotIndex: index });
-    game.players.manager.makeNewAttacker(this);
-    const newDefender = game.players.manager.makeDefender(this.left);
-    return game.round.pushNextMove(DefenderMove, { player: newDefender });
-  }
-
-  stopMove({ game }: { game: DurakGame }): void {
-    if (!game.desk.isDefended) {
-      // here defender can not defend more
-      // if desk is full THAN go to next round
-      // OR THAN let put more cards in pursuit (vdogonku)
-      game.round.updateCurrentMoveTo(DefenderGaveUpMove);
-      return game.desk.allowsMoves
-        ? this.letPrimalAttackerMove({ game })
-        : game.handleLostDefence(this);
-    }
-    // desk is defended
-    // let primal attacker try make defender loser
-    game.round.updateCurrentMoveTo(StopDefenseMove);
-    return this.letPrimalAttackerMove({ game });
-  }
-
-  private putTransferCard({ game, card, slotIndex }: { game: DurakGame, card: Card, slotIndex: number }) {
-    this.putCard({ game, card, slotIndex }, TransferMove);
-  }
-
-  private letPrimalAttackerMove({ game }: { game: DurakGame }) {
-    const primalAttacker = game.players.manager.makeNewAttacker(game.round.primalAttacker);
-    return game.round.pushNextMove(AttackerMove, { player: primalAttacker });
-  }
-
-  private putCard<M extends TransferMove | InsertDefendCardMove>(
-    { game, card, slotIndex }: { game: DurakGame, card: Card, slotIndex: number },
-    Move: { new(arg: any): M },
-  ) {
-    this.remove({ card });
-    game.round.updateCurrentMoveTo(Move);
-    game.desk.receiveCard({ card, index: slotIndex, who: this });
+  canWinDefense({ game }: { game: DurakGame }) {
+    return (
+      game.desk.isDefended
+      && game.round.hasPrimalAttacker
+      && (
+        //  below statement is for 2 players game:
+        //  in 2 players game can be only one attacker
+        //  IF attacker stop move THAN defender won
+        this.left.isPrimalAttacker({ round: game.round })
+        // below statement is for more than 2 players game
+        || game.players.attacker.left.isPrimalAttacker({ round: game.round })
+      )
+    );
   }
 }
