f41a6db0ec27f5b22b11acd0a496b7ef2edb72ee
GeForsen- <semen.neb@gmail.com>
2023-05-17 16:49:19 +0700

    handlePutCardOnDesk: update param name

--- src/namespaces/games/methods/handle-put-card-on-desk.ts
+++ src/namespaces/games/methods/handle-put-card-on-desk.ts
@@ -6,18 +6,18 @@ import assert from "node:assert";
 export default async function handlePutCardOnDesk(
   this: { game: DurakGame, playerId: string },
   { suit, rank }: CardDTO,
-  index: number,
+  slotIndex: number,
 ) {
   const { game, playerId: id } = this;
   const player = getPlayer(game, id);
   const card = getPlacedCard({ suit, rank }, game, player);
   if (
     game.players.isDefender(player)
-    && await game.desk.allowsTransferMove({ index, card, nextDefender: player.left })
+    && await game.desk.allowsTransferMove({ index: slotIndex, card, nextDefender: player.left })
   ) {
     return player.makeTransferMove({ game, index, card });
   }
   assert.ok(game.players.isSuperPlayer(player), "У вас нет прав ложить карту на стол");
   clearTimeout(game.round.currentMove.defaultBehaviour);
-  return await game.round.currentMove.putCardOnDesk(card, index);
+  return await game.round.currentMove.putCardOnDesk(card, slotIndex);
 }
