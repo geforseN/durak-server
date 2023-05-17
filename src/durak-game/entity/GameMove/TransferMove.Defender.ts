d1c8b9dfad038ec0ce59b3ca7f68171680d9286b
GeForsen- <semen.neb@gmail.com>
2023-05-17 15:02:00 +0700

    TransferMove: add logic

--- src/durak-game/entity/GameMove/TransferMove.Defender.ts
+++ src/durak-game/entity/GameMove/TransferMove.Defender.ts
@@ -1,4 +1,31 @@
 import { DefenderMove } from "./DefenderMove";
+import Card from "../Card";
+
+type ConstructorArg =
+  ConstructorParameters<typeof DefenderMove>[number]
+  & { card: Card, slotIndex: number };
 
 export class TransferMove extends DefenderMove {
+  card: Card;
+  slotIndex: number;
+
+  constructor({ card, slotIndex, ...arg }: ConstructorArg) {
+    super(arg);
+    this.card = this.player.remove({ card });
+    this.slotIndex = slotIndex;
+    this.#insertTransferCardOnDesk();
+  }
+
+  #insertTransferCardOnDesk() {
+    return this.game.desk.receiveCard({
+      card: this.card,
+      index: this.slotIndex,
+      who: this.player,
+    });
+  }
+
+  handleAfterTransfer() {
+    this.game.players.manager.makeNewAttacker(this.player);
+    return this.game.round.giveAttackerLeftDefend();
+  }
 }
