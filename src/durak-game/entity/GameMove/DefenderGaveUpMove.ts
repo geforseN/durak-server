73260440ed1542e6e1bb82848e4f82a90e83c511
GeForsen- <semen.neb@gmail.com>
2023-05-17 15:21:06 +0700

    DefenderGaveUpMove.handleAfterGaveUp: init

--- src/durak-game/entity/GameMove/DefenderGaveUpMove.ts
+++ src/durak-game/entity/GameMove/DefenderGaveUpMove.ts
@@ -1,4 +1,9 @@
 import { DefenderMove } from "./DefenderMove";
 
 export class DefenderGaveUpMove extends DefenderMove {
+  handleAfterGaveUp() {
+    return this.game.desk.allowsMoves
+      ? this.game.round.givePrimalAttackerAttack()
+      : this.game.handleLostDefence(this.player);
+  }
 }
