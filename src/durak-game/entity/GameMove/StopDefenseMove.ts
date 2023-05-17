4ad87d4c1dda78b1ee53931018f6accbb9cf2ef3
GeForsen- <semen.neb@gmail.com>
2023-05-17 15:10:28 +0700

    StopDefenseMove.handleAfterStopMove: init

--- src/durak-game/entity/GameMove/StopDefenseMove.ts
+++ src/durak-game/entity/GameMove/StopDefenseMove.ts
@@ -1,4 +1,10 @@
 import { DefenderMove } from "./DefenderMove";
 
 export class StopDefenseMove extends DefenderMove {
+  handleAfterStopMove() {
+    if (!this.game.desk.isDefended) {
+      this.game.round.makeDefenderLost();
+    }
+    return this.game.round.givePrimalAttackerAttack();
+  }
 }
