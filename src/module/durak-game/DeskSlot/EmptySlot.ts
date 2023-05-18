1a7b267d7763927e9b9f63db5d3fa46f6097f237
GeForsen- <semen.neb@gmail.com>
2023-05-18 18:28:37 +0700

    EmptySlot: add override

--- src/module/durak-game/DeskSlot/EmptySlot.ts
+++ src/module/durak-game/DeskSlot/EmptySlot.ts
@@ -10,15 +10,15 @@ export default class EmptySlot extends DeskSlot {
     return [];
   }
 
-  ensureCanBeAttacked({ card }: { card: Card }) {
+  override ensureCanBeAttacked({ card }: { card: Card }) {
     return Promise.resolve(card);
   }
 
-  ensureCanBeDefended({ card: _card }: { card: Card }) {
+  override ensureCanBeDefended({ card: _card }: { card: Card }) {
     return Promise.reject(new Error("Нет от чего защищаться"));
   }
 
-  ensureAllowsTransfer({ card }: { card: Card }) {
+  override ensureAllowsTransfer({ card }: { card: Card }) {
     return Promise.resolve(card);
   }
 }
