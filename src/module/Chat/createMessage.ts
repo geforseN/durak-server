821433c987e37ce88fdfe4ffd669aa9f52e2a516
GeForsen- <semen.neb@gmail.com>
2023-06-04 02:25:11 +0700

    createMessage: init

---
+++ src/module/Chat/createMessage.ts
@@ -0,0 +1,13 @@
+import getUserForChat from "../../../prisma/user/getUserForChat";
+import { randomUUID } from "crypto";
+import { ChatMessage, ChatReplyMessage } from "./entity";
+
+export default function createMessage({ sender, text, replyMessageId }: {
+  sender: Awaited<ReturnType<typeof getUserForChat>>,
+  text: string,
+  replyMessageId?: ReturnType<typeof randomUUID>
+}) {
+  return !replyMessageId
+    ? new ChatMessage({ sender, text })
+    : new ChatReplyMessage({ sender, text, replyMessageId });
+}
