57a10e7feae8b2b417e651ceb0a528f23a85960e
GeForsen- <semen.neb@gmail.com>
2023-06-06 18:52:59 +0700

    delete globalChat.namespace related files

--- src/namespaces/global-chat/check-text-length.ts
+++
@@ -1,8 +0,0 @@
-export default function checkTextLength(text: string): void | never {
-  if (!text.length) {
-    throw new Error("Нельзя прислать пустое сообщение");
-  }
-  if (text.length >= 128) {
-    throw new Error("Длинна сообщения превышает 128");
-  }
-}
