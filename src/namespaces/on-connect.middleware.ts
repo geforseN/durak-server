77067535e089bf114ac2cd5a51f595e3414b3683
GeForsen- <semen.neb@gmail.com>
2023-06-06 18:53:16 +0700

    move onConnectMiddleware

---
+++ src/namespaces/on-connect.middleware.ts
@@ -0,0 +1,12 @@
+import { parse } from "cookie";
+import { LobbiesIO } from "./lobbies/lobbies.types";
+import { ExtendedError } from "socket.io/dist/namespace";
+
+export default function onConnectMiddleware(socket: LobbiesIO.SocketIO, next: (err?: (ExtendedError | undefined)) => void) {
+  if (!socket.handshake.headers.cookie) return next();
+  const { sid } = parse(socket.handshake.headers.cookie);
+  if (!sid) return next();
+  socket.data.sid = sid;
+  socket.join(sid);
+  return next();
+}
