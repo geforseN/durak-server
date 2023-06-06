a679ebb294cabb6972b8c64be4a49022712be905
GeForsen- <semen.neb@gmail.com>
2023-06-06 19:05:55 +0700

    initializeChat: refactor logic with ws helpers & change return signature

--- src/module/Chat/initializeChatNamespace.ts
+++ src/module/Chat/initializeChatNamespace.ts
@@ -1,51 +1,33 @@
 import { randomUUID } from "crypto";
+import { FastifyRequest } from "fastify";
+import { SocketStream } from "@fastify/websocket";
 import WebSocket from "ws";
 import { Chat } from "./entity";
-import { SocketStream } from "@fastify/websocket";
-import { FastifyRequest } from "fastify";
-import assert from "node:assert";
+import {
+  dispatchMessageToCertainListener,
+  emitSocketOnce,
+  emitEverySocketOn,
+  emitSocketOn,
+  addUserSocketInRoom,
+} from "../../ws";
 
 export default function initializeChat() {
   type UserId = string | ReturnType<typeof randomUUID>
-  const userSockets = new Map<UserId, Set<WebSocket>>();
+  const userSocketRooms = new Map<UserId, Set<WebSocket>>();
   const sockets = <WebSocket[]>[];
   const globalChat = new Chat();
 
-  function handleConnection(connection: SocketStream, request: FastifyRequest) {
+  return function getChatContext(connection: SocketStream, request: FastifyRequest) {
     sockets.push(connection.socket);
-    const { userId } = request.session.userProfile;
-    if (!userSockets.has(userId)) {
-      userSockets.set(userId, new Set([connection.socket]));
-    } else {
-      userSockets.get(userId)?.add(connection.socket);
-    }
-
-    connection.socket.on(
-      "socket",
-      (eventName: "history__restore" | "notification__send", ...payload: any[]) => {
-        connection.socket.send(JSON.stringify({ eventName, payload }));
-      },
-    );
-
-    connection.socket.on(
-      "everySocket",
-      (eventName: "message__send", ...payload: any[]) => {
-        const message = JSON.stringify({ eventName, payload });
-        sockets.forEach((socket) => {
-          socket.send(message);
-        });
-      },
-    );
-
-    connection.socket.onmessage = (event) => {
-      assert.ok(typeof event.data === "string");
-      const { eventName, payload } = JSON.parse(event.data);
-      connection.socket.emit(eventName, ...payload);
+    addUserSocketInRoom(request, connection, userSocketRooms);
+    dispatchMessageToCertainListener(connection);
+    emitSocketOn<"notification__send">(connection.socket);
+    emitSocketOnce<"history__restore">(connection.socket);
+    emitEverySocketOn<"message__send">(connection.socket, sockets);
+    return {
+      userId: request.session.userProfile.userId,
+      socket: connection.socket,
+      chat: globalChat,
     };
-  }
-
-  return {
-    globalChat,
-    handleConnection,
   };
 }
