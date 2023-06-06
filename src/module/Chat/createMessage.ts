092631c773337e69988622a71f8b0f849ebf921a
GeForsen- <semen.neb@gmail.com>
2023-06-06 19:50:24 +0700

    createMessage: expand replyMessageId type

--- src/module/Chat/createMessage.ts
+++ src/module/Chat/createMessage.ts
@@ -5,7 +5,7 @@ import { ChatMessage, ChatReplyMessage } from "./entity";
 export default function createMessage({ sender, text, replyMessageId }: {
   sender: Awaited<ReturnType<typeof getUserForChat>>,
   text: string,
-  replyMessageId?: ReturnType<typeof randomUUID>
+  replyMessageId?: string | ReturnType<typeof randomUUID>
 }) {
   return !replyMessageId
     ? new ChatMessage({ sender, text })
