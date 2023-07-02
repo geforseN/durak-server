46316c46bfa17e00a7518812e235daa7aec13b50
GeForsen- <semen.neb@gmail.com>
2023-07-02 20:41:36 +0700

    handlers: use player instances getter & lint

--- src/module/DurakGame/socket/handler/handle-stop-move.ts
+++ src/module/DurakGame/socket/handler/handle-stop-move.ts
@@ -1,15 +1,15 @@
-import { getPlayer } from "../getter";
 import assert from "node:assert";
+import { getPlayer } from "../getter";
 import DurakGame from "../../DurakGame.implimetntation";
 
-export default function handleStopMove(
-  this: { game: DurakGame, playerId: string }
-) {
-  const { game, playerId: id } = this;
-  const player = getPlayer(game, id);
+export default function handleStopMove(this: {
+  game: DurakGame;
+  playerId: string;
+}) {
+  const { game, playerId } = this;
+  const player = getPlayer(game, playerId);
   assert.ok(!game.desk.isEmpty, "Нельзя закончить раунд с пустым столом");
-  assert.ok(game.players.isSuperPlayer(player), "Нет права закончить ход");
+  assert.ok(player.isSuperPlayer, "Нет права закончить ход");
   clearTimeout(game.round.currentMove.defaultBehaviour);
   game.round.currentMove.stopMove();
 }
-
