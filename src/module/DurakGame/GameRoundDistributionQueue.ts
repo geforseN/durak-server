0d6f2ee15fc43c13b5d301d615363c7e8bda0bbc
GeForsen- <semen.neb@gmail.com>
2023-07-05 21:39:00 +0700

    GameRoundDistributionQueue: refactor
    remove mapping in this.makeDistribution
    make this.playersQueue getter
    refactor this.makeInitialDistribution

--- src/module/DurakGame/GameRoundDistributionQueue.ts
+++ src/module/DurakGame/GameRoundDistributionQueue.ts
@@ -1,62 +1,41 @@
+import assert from "node:assert";
 import type DurakGame from "./DurakGame.implimetntation";
 import { Player } from "./entity/Player";
 
-// TODO refactor
 export default class GameRoundDistributionQueue {
   game: DurakGame;
-  playersQueue: Player[];
 
   constructor(game: DurakGame) {
     this.game = game;
-    // TODO refactor
-    this.playersQueue = this.#makeDistributionQueue();
   }
 
-  #makeDistributionQueue() {
-    const playersQueue: Player[] = [this.game.players.defender.right];
-    let player = this.game.players.defender.left;
-    while (player.id !== this.game.round.primalAttacker.id) {
+  get playersQueue() {
+    const { primalAttacker } = this.game.round;
+    const playersQueue: Player[] = [primalAttacker];
+    assert.strictEqual(primalAttacker.left, this.game.players.defender);
+    let player = primalAttacker.left as Player;
+    while ((player = player.left) !== primalAttacker) {
       playersQueue.push(player);
-      player = player.left;
     }
-    playersQueue.push(this.game.players.defender);
+    playersQueue.push(primalAttacker.left);
     return playersQueue;
   }
 
   makeDistribution() {
-    const distributionQueue = this.playersQueue
-      // TODO remove mapping if assert will never happen
-      .map((distributionPlayer) => {
-        const player = this.game.players.get(
-          (player) => player.id === distributionPlayer.id,
-        );
-        console.assert(
-          distributionPlayer === player,
-          "DISTRIBUTION %s QUEUE",
-          player.id,
-        );
-        return player;
-      });
-    for (const player of distributionQueue) {
+    for (const player of this.playersQueue) {
       if (this.game.talon.isEmpty) return;
       this.game.talon.provideCards(player);
     }
   }
 
-  // TODO refactor
   makeInitialDistribution() {
-    const { finalCardCount, cardCountPerIteration: cardCount } =
+    const { finalCardCount, cardCountPerIteration } =
       this.game.settings.initialDistribution;
-    const finalCardCountToDistribute = this.game.players.count * finalCardCount;
-    for (
-      let givenCardCount = 0;
-      givenCardCount < finalCardCountToDistribute;
-      givenCardCount += cardCount
-    ) {
-      const playerIndex =
-        (givenCardCount / cardCount) % this.game.players.count;
-      const player = [...this.game.players][playerIndex];
-      this.game.talon.provideCards(player, cardCount);
+    const numberOfIterations = finalCardCount / cardCountPerIteration;
+    let [player] = this.game.players;
+    assert.ok(player);
+    for (let i = 0; i < numberOfIterations; i++, player = player.left) {
+      this.game.talon.provideCards(player, cardCountPerIteration);
     }
   }
 }
