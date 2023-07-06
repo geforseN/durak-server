833eea4ae69aeb0fd2cd4a2b9f1dec5e8b93f76b
GeForsen- <semen.neb@gmail.com>
2023-07-06 22:40:23 +0700

    Player: make ctor ugly & remove changeKindTo

--- src/module/DurakGame/entity/Player/Player.ts
+++ src/module/DurakGame/entity/Player/Player.ts
@@ -1,7 +1,8 @@
+import type { LobbyUser } from "../../../Lobbies/lobbies.namespace";
+import type Card from "../Card";
 import { Hand } from "../Deck";
-import Card from "../Card";
 import GamePlayerWebsocketService from "./Player.service";
-import { LobbyUser } from "../../../Lobbies/lobbies.namespace";
+import SidePlayersIndexes from "./SidePlayersIndexes";
 
 export const GOOD_CARD_AMOUNT = 6 as const;
 const allowedMissingCardCount = [0, 1, 2, 3, 4, 5, 6] as const;
@@ -19,22 +20,41 @@ export default class Player {
   constructor(lobbyUser: LobbyUser);
   constructor(player: Player);
   constructor(player: Player, wsService: GamePlayerWebsocketService);
+  constructor(player: Player, index: number, players: Player[]);
   constructor(
     playerOrLobbyUser: LobbyUser | Player,
-    wsService?: GamePlayerWebsocketService,
+    wsServiceOrIndex?: GamePlayerWebsocketService | number,
+    players?: Player[],
   ) {
-    if (playerOrLobbyUser instanceof Player) {
-      const player = playerOrLobbyUser;
-      this.hand = player.hand;
-      this.info = player.info;
-      this.left = player.left;
-      this.right = player.right;
-      this.wsService = wsService || player.wsService;
-      this.left.right = this;
-      this.right.left = this;
-    } else {
+    if (!(playerOrLobbyUser instanceof Player)) {
+      const lobbyUser = playerOrLobbyUser;
       this.hand = new Hand();
-      this.info = playerOrLobbyUser;
+      this.info = lobbyUser;
+    } else {
+      const player = playerOrLobbyUser;
+      if (wsServiceOrIndex instanceof GamePlayerWebsocketService) {
+        this.info = player.info;
+        this.hand = player.hand;
+        const wsService = wsServiceOrIndex;
+        this.wsService = wsService;
+      } else if (wsServiceOrIndex && players) {
+        this.hand = player.hand;
+        this.info = player.info;
+        this.wsService = player.wsService;
+        const index = wsServiceOrIndex;
+        const indexes = new SidePlayersIndexes(index, players.length);
+        this.left = players[indexes.leftPlayerIndex];
+        this.right = players[indexes.rightPlayerIndex];
+      } else {
+        this.info = player.info;
+        this.hand = player.hand;
+        this.left = player.left;
+        this.right = player.right;
+        this.wsService = player.wsService;
+        this.left.right = this;
+        this.right.left = this;
+        this.wsService?.emitOwnKind(this);
+      }
     }
   }
 
@@ -74,9 +94,5 @@ export default class Player {
     this.left.right = this.right;
     this.right.left = this.left;
     return this.wsService?.exitGame(this);
-  }
-
-  changeKindTo(newKind: PlayerKind | typeof Player) {
-    this.wsService?.changeKind(newKind, this);
   }
 }
