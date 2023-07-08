c8a1ee54b11adf9fdd48c8b86e1f85a04421055c
GeForsen- <semen.neb@gmail.com>
2023-07-08 17:53:31 +0700

    lobby errors: add default messages

--- src/module/Lobbies/error.ts
+++ src/module/Lobbies/error.ts
@@ -1,20 +1,20 @@
 export class DeleteLobbyError extends Error {
-  constructor() {
-    super("Нет такого лобби");
+  constructor(message = "Нет такого лобби") {
+    super(message);
     this.name = "Ошибка удаления лобби";
   }
 }
 
 export class LobbyAccessError extends Error {
-  constructor(message: string = "Вы не являетесь админом лобби") {
+  constructor(message = "Вы не являетесь админом лобби") {
     super(message);
     this.name = "Ошибка доступа";
   }
 }
 
 export class FindLobbyError extends Error {
-  constructor(message?: string) {
-    super(message || "Лобби не найдено");
+  constructor(message = "Лобби не найдено") {
+    super(message);
     this.name = "Ошибка нахождения лобби";
   }
 }
