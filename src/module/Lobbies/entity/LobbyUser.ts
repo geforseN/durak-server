710a52c10f3ecb6d6658b449fa88e3cec0abd2a5
GeForsen- <semen.neb@gmail.com>
2023-07-08 17:32:56 +0700

    LobbyUser: remove static

--- src/module/Lobbies/entity/LobbyUser.ts
+++ src/module/Lobbies/entity/LobbyUser.ts
@@ -4,8 +4,4 @@ export default class LobbyUser {
   id: string = randomUUID(); // use prisma.user.id
   isAdmin: boolean = false;
   nickname: string = "";
-
-  static hasSameId(this: { id: string }, user?: LobbyUser) {
-    return this.id === user?.id;
-  }
 }
