724067121a0b51f5989188e6db99693cd47a0de3
GeForsen- <semen.neb@gmail.com>
2023-07-08 21:53:32 +0700

    Lobby: update

--- src/module/Lobbies/entity/Lobby.ts
+++ src/module/Lobbies/entity/Lobby.ts
@@ -4,6 +4,9 @@ import CorrectGameSettings, { type GameSettings } from "./CorrectGameSettings";
 import type EventEmitter from "events";
 import LobbySlots from "./LobbySlots";
 import { getFirstTimeUser, type LobbyUser } from "../lobbies.namespace";
+import { LobbyAccessError } from "../error";
+import { durakGames } from "../../..";
+import { UnstartedGame } from "../../DurakGame/NonstartedDurakGame";
 
 export default class Lobby {
   id: string;
@@ -13,12 +16,12 @@ export default class Lobby {
 
   constructor({
     settings,
-    lobbiesEmitter: bestEmitter,
+    lobbiesEmitter,
   }: {
     settings: GameSettings;
     lobbiesEmitter: EventEmitter;
   }) {
-    this.lobbiesEmitter = bestEmitter;
+    this.lobbiesEmitter = lobbiesEmitter;
     this.id = crypto.randomUUID();
     this.settings = new CorrectGameSettings(settings);
     this.slots = new LobbySlots({
@@ -39,20 +42,12 @@ export default class Lobby {
     return this.slots.isFull;
   }
 
-  has(cb: (user?: LobbyUser) => boolean): boolean {
-    return this.slots.has(cb);
+  hasUser(userId: string) {
+    return this.slots.has((user) => user?.id === userId);
   }
 
   get admin() {
     return this.slots.admin;
-  }
-
-  updateAdmin() {
-    this.slots.admin = this.slots.mostLeftSideNonAdminUser;
-    this.lobbiesEmitter.emit("everySocket", "lobby::admin::update", {
-      adminId: this.slots.admin.id,
-      lobbyId: this.id,
-    });
   }
 
   insertUser(user: LobbyUser, slotIndex: number) {
@@ -64,14 +59,23 @@ export default class Lobby {
     });
   }
 
-  removeUser(userId: string) {
+  tryUpdateToUnstartedGame(userId: string) {
+    assert.ok(this.admin.id === userId, new LobbyAccessError());
+    durakGames.set(this.id, new UnstartedGame(this));
+  }
+
+  removeUserWithEmit(userId: string) {
     const user = this.slots.remove((user) => user?.id === userId);
     if (this.isEmpty) {
       this.lobbiesEmitter.emit("everySocket", "lobby::delete", {
         lobbyId: this.id,
       });
     } else if (user.isAdmin) {
-      this.updateAdmin();
+      this.slots.admin = this.slots.mostLeftSideNonAdminUser;
+      this.lobbiesEmitter.emit("everySocket", "lobby::admin::update", {
+        adminId: this.slots.admin.id,
+        lobbyId: this.id,
+      });
     }
     return user;
   }
