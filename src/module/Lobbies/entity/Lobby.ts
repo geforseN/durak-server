b612d52dd8cb8a5051089c20aed0ea052bc017b9
GeForsen- <semen.neb@gmail.com>
2023-07-10 21:25:26 +0700

    add trash

--- src/module/Lobbies/entity/Lobby.ts
+++ src/module/Lobbies/entity/Lobby.ts
@@ -3,7 +3,7 @@ import crypto from "node:crypto";
 import CorrectGameSettings, { type GameSettings } from "./CorrectGameSettings";
 import type EventEmitter from "events";
 import LobbySlots from "./LobbySlots";
-import { getFirstTimeUser, type LobbyUser } from "../lobbies.namespace";
+import { type LobbyUser } from "../lobbies.namespace";
 import { LobbyAccessError } from "../error";
 import { durakGames } from "../../..";
 import { UnstartedGame } from "../../DurakGame/NonstartedDurakGame";
@@ -11,7 +11,7 @@ import { UnstartedGame } from "../../DurakGame/NonstartedDurakGame";
 export default class Lobby {
   id: string;
   settings: GameSettings;
-  slots: LobbySlots<Awaited<ReturnType<typeof getFirstTimeUser>>>;
+  slots: LobbySlots;
   lobbiesEmitter: EventEmitter;
 
   constructor({
@@ -28,6 +28,28 @@ export default class Lobby {
       size: settings.userCount,
       emitter: this.lobbiesEmitter,
     });
+    this.lobbiesEmitter.emit("lobby##add", { lobby: this });
+  }
+
+  assertSlotIndex(slotIndex: number) {
+    if (!Number.isInteger(slotIndex)) throw 1;
+    if (slotIndex < -1) throw 2;
+    if (slotIndex > this.settings.userCount) throw 3;
+  }
+  
+  ensureIsAdmin(userId: string) {
+    assert.ok(this.admin.id === userId, new LobbyAccessError());
+  }
+  
+  ensureCanJoin(userId: string, slotIndex: number) {
+    assert.ok(!this.isFull, new LobbyAccessError("Лобби полностью занято"));
+    // NOTE: fast return below but still should handle -1 later
+    if (slotIndex === -1) return;
+    const slotToJoin = this.slots.value[slotIndex];
+    const userSlot = this.slots.ABSTRACT__findUser(userId);
+    assert.ok(slotToJoin.isEmpty);
+    if (!userSlot) return;
+    assert.ok(userSlot.index !== slotToJoin, "Данный слот уже занят вами");
   }
 
   get value() {
@@ -43,60 +65,110 @@ export default class Lobby {
   }
 
   hasUser(userId: string) {
-    return this.slots.has((user) => user?.id === userId);
+    return this.slots.hasUser(userId);
   }
 
   get admin() {
     return this.slots.admin;
   }
 
+  @insertUserwithEmit
   insertUser(user: LobbyUser, slotIndex: number) {
-    const filledSlotIndex = this.slots.putUser(user, slotIndex);
-    this.lobbiesEmitter.emit("lobby::user::put", {
-      lobbyId: this.id,
-      user,
-      slotIndex: filledSlotIndex,
-    });
+    return this.slots.putUser(user, slotIndex);
   }
 
-  tryUpdateToUnstartedGame(userId: string) {
-    assert.ok(this.admin.id === userId, new LobbyAccessError());
+  updateToUnstartedGame() {
     durakGames.set(this.id, new UnstartedGame(this));
+    this.lobbiesEmitter.emit("lobby##remove", { lobbyId: this.id });
   }
 
-  removeUserWithEmit(userId: string) {
-    const user = this.slots.remove((user) => user?.id === userId);
+  @removeUserWithEmits
+  removeUser(userId: string) {
+    return this.slots.removeUser(userId);
+  }
+
+  @moveUserWithEmit
+  moveUser(userId: string, desiredSlotIndex: number) {
+    const userSlotIndex = this.slots.getSlotIndexOfUser(userId);
+    assert.ok(userSlotIndex !== desiredSlotIndex, "Данный слот уже занят вами");
+    this.slots.swap(userSlotIndex, desiredSlotIndex);
+    return userSlotIndex;
+  }
+}
+
+function removeUserWithEmits<
+  This extends Lobby,
+  Args extends Parameters<Lobby["removeUser"]>,
+  Return extends ReturnType<Lobby["removeUser"]>,
+>(
+  target: (this: This, ...args: Args) => Return,
+  __context__: ClassMethodDecoratorContext<
+    This,
+    (this: This, ...args: Args) => Return
+  >,
+) {
+  return function (this: This, ...args: Args): Return {
+    const removedUser = target.apply(this, args);
     if (this.isEmpty) {
-      this.lobbiesEmitter.emit("everySocket", "lobby::delete", {
+      /* TODO in lobbies remove this lobby from #map */
+      this.lobbiesEmitter.emit("lobby##remove", {
         lobbyId: this.id,
       });
-    } else if (user.isAdmin) {
+    } else if (removedUser.isAdmin) {
       this.slots.admin = this.slots.mostLeftSideNonAdminUser;
       this.lobbiesEmitter.emit("everySocket", "lobby::admin::update", {
         adminId: this.slots.admin.id,
         lobbyId: this.id,
       });
     }
-    return user;
-  }
+    return removedUser;
+  };
+}
 
-  moveUser(userId: string, desiredSlotIndex: number) {
-    const currentSlotIndex = this.slots.findSlotIndex(
-      (user) => user?.id === userId,
-    );
-    assert.ok(
-      currentSlotIndex !== desiredSlotIndex,
-      "Данный слот уже занят вами",
-    );
-    const slot = this.slots.slotAt(desiredSlotIndex);
-    assert.ok(slot.isValid && slot.isEmpty);
-    this.slots.swap(currentSlotIndex, desiredSlotIndex);
+function moveUserWithEmit<
+  This extends Lobby,
+  Args extends Parameters<Lobby["moveUser"]>,
+  Return extends ReturnType<Lobby["moveUser"]>,
+>(
+  target: (this: This, ...args: Args) => Return,
+  __context__: ClassMethodDecoratorContext<
+    This,
+    (this: This, ...args: Args) => Return
+  >,
+) {
+  return function (this: This, ...args: Args): Return {
+    const [userId, newSlotIndex] = args;
+    const pastSlotIndex = target.apply(this, args);
     this.lobbiesEmitter.emit("everySocket", "lobby::user::move", {
       lobbyId: this.id,
       userId,
-      newSlotIndex: desiredSlotIndex,
+      newSlotIndex,
       // NOTE: property below can be omited
-      pastSlotIndex: currentSlotIndex,
+      pastSlotIndex,
     });
-  }
+    return pastSlotIndex;
+  };
+}
+
+function insertUserwithEmit<
+  This extends Lobby,
+  Args extends Parameters<Lobby["insertUser"]>,
+  Return extends ReturnType<Lobby["insertUser"]>,
+>(
+  target: (this: This, ...args: Args) => Return,
+  __context__: ClassMethodDecoratorContext<
+    This,
+    (this: This, ...args: Args) => Return
+  >,
+) {
+  return function (this: This, ...args: Args): Return {
+    const [user] = args;
+    const filledSlotIndex = target.apply(this, args);
+    this.lobbiesEmitter.emit("everySocket", "lobby::user::put", {
+      lobbyId: this.id,
+      user,
+      slotIndex: filledSlotIndex,
+    });
+    return filledSlotIndex;
+  };
 }
