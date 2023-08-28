813b412f3bf27a59ff803686c0da3c11d63000fc
geforseN <semen.neb@gmail.com>
2023-08-28 21:50:46 +0700

    Hand: simplify and move deleted to SuperHand

---
+++ src/module/DurakGame/entity/Deck/Hand/SuperHand.ts
@@ -0,0 +1,25 @@
+import Card from "../../Card";
+import Hand from ".";
+import assert from "node:assert";
+import crypto from "node:crypto";
+
+export default class SuperHand extends Hand {
+  constructor(cards: Card[]) {
+    super(cards);
+  }
+
+  get randomCard() {
+    return this.value[crypto.randomInt(this.count)];
+  }
+
+  remove(
+    cb: (card: Card) => boolean,
+    notRemovedMessage = "Не получилось убрать свою карту",
+  ): Card {
+    const index = this.value.findIndex(cb);
+    assert.ok(index >= 0, notRemovedMessage);
+    const [removedCard] = this.value.splice(index, 1);
+    assert.ok(removedCard, notRemovedMessage);
+    return removedCard;
+  }
+}
