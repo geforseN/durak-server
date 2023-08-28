25e7d213e11387146eac33eb7f2ed7c3bf74cb5c
geforseN <semen.neb@gmail.com>
2023-08-28 22:50:26 +0700

    fix NotificationAlert

--- src/module/NotificationAlert/index.ts
+++ src/module/NotificationAlert/index.ts
@@ -1,25 +1,23 @@
 import crypto from "node:crypto";
 import { NotificationAlert as TNotificationAlert } from "@durak-game/durak-dts";
+
 export default class NotificationAlert {
   message;
   type;
   durationInMS;
   id;
 
-  constructor(
-    {
-      message = "Произошла ошибка",
-      durationInMS = 5_000,
-      type = "Warning",
-      id = crypto.randomUUID(),
-      ...data // TODO maybe can work not how wanted
-    }: Partial<TNotificationAlert> = {
-    },
-  ) {
-    this.message = message;
-    // TODO maybe can work not how wanted
-    this.type = data instanceof Error ? "Error" : type;
-    this.durationInMS = durationInMS;
-    this.id = id;
+  constructor(data: Partial<TNotificationAlert> = {}) {
+    data.message ??= "Произошла ошибка";
+    data.durationInMS ??= 5_000;
+    data.id ??= crypto.randomUUID();
+    this.message = data.message;
+    this.type = data.type
+      ? data.type
+      : data instanceof Error
+      ? "Error"
+      : "Warning";
+    this.durationInMS = data.durationInMS;
+    this.id = data.id;
   }
 }
