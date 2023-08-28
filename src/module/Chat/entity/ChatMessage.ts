13cd69d6e08e913b88d8ecad45886e61966d6a7a
geforseN <semen.neb@gmail.com>
2023-08-28 17:16:46 +0700

    add message type guard and update imports

--- src/module/Chat/entity/ChatMessage.ts
+++ src/module/Chat/entity/ChatMessage.ts
@@ -1,16 +1,21 @@
-import { randomUUID } from "crypto";
-import { ChatContext } from "../chatPlugin";
+import crypto from "node:crypto";
+import type { ChatContext } from "../chatPlugin";
+import type ChatReplyMessage from "./ChatReplyMessage";
 
 export default class ChatMessage {
   sender: ChatContext["sender"];
   date: number;
   text: string;
-  id: ReturnType<typeof randomUUID>;
+  id: string;
 
   constructor(props: { sender: ChatContext["sender"]; text: string }) {
     this.date = new Date().getTime();
     this.sender = props.sender;
     this.text = props.text;
-    this.id = randomUUID();
+    this.id = crypto.randomUUID();
+  }
+
+  isReplyMessage(): this is ChatReplyMessage {
+    return false;
   }
 }
