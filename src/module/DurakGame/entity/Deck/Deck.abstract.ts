1b08bdd52c026a4b0ca9f357e6db0d60a1c6a07d
geforseN <semen.neb@gmail.com>
2023-08-28 21:47:33 +0700

    Deck: remove deck build methods

--- src/module/DurakGame/entity/Deck/Deck.abstract.ts
+++ src/module/DurakGame/entity/Deck/Deck.abstract.ts
@@ -5,8 +5,8 @@ export type CardCount = 24 | 36 | 52;
 export default abstract class Deck {
   protected value: Card[];
 
-  protected constructor(size?: CardCount) {
-    this.value = this.#buildDeck(size);
+  protected constructor(cards: Card[] = []) {
+    this.value = cards;
   }
 
   *[Symbol.iterator]() {
@@ -19,20 +19,5 @@ export default abstract class Deck {
 
   get isEmpty() {
     return this.value.length === 0;
-  }
-
-  #availableRanks(rankCount: number) {
-    return [...Card.ranks]
-      .reverse()
-      .filter((_rank, index) => index < rankCount);
-  }
-
-  #buildDeck(size?: CardCount): Card[] {
-    if (!size) return [];
-    const rankCount = Math.floor(size / Card.suits.length);
-    const availableRanks = this.#availableRanks(rankCount);
-    return Card.suits.flatMap((suit) =>
-      availableRanks.map((rank) => new Card({ rank, suit })),
-    );
   }
 }
