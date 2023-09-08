98ce7c67d58c46eec20891481cbf40c5149b6bdd
geforseN <semen.neb@gmail.com>
2023-09-08 11:21:54 +0700

    DurakGameStateDTO: use toJSON  and update data

--- src/module/DurakGame/DTO/DurakGameState.dto.ts
+++ src/module/DurakGame/DTO/DurakGameState.dto.ts
@@ -1,10 +1,5 @@
 import type DurakGame from "../DurakGame";
-import SelfDTO from "./Self.dto";
-import EnemyDTO from "./Enemy.dto";
-import OrderedPlayerEnemies from "../entity/Players/OrderedPlayerEnemies";
-import type { Player } from "../entity/__Player";
-import { isPlayerKind } from "@durak-game/durak-dts";
-import assert from "node:assert";
+import type { BasePlayer } from "../entity/Player/BasePlayer.abstract";
 
 export default class DurakGameStateDto {
   self;
@@ -14,37 +9,20 @@ export default class DurakGameStateDto {
   talon;
   discard;
   round;
-  isGameEnded;
+  status;
 
-  constructor(game: DurakGame, playerId: Player["id"]) {
+  // NOTE: even if game.status === 'ended'
+  // all properties above should be correctly added
+  // no errors should be received while this object is being created
+  constructor(game: DurakGame, playerId: BasePlayer["id"]) {
     const currentPlayer = game.players.get((player) => player.id === playerId);
-    this.self = new SelfDTO(currentPlayer);
-    this.enemies = new OrderedPlayerEnemies(currentPlayer).value.map(
-      (enemy) => new EnemyDTO(enemy),
-    );
+    this.self = currentPlayer.toSelf();
+    this.enemies = currentPlayer.enemies;
     this.settings = game.settings;
-    // TODO implement toJSON method in talon, discard, round, ...etc
-    this.desk = { slots: [...game.desk] };
-    this.talon = {
-      trumpCard: game.talon.trumpCard,
-      isEmpty: game.talon.isEmpty,
-      hasOneCard: game.talon.hasOneCard,
-    };
-    this.discard = { isEmpty: game.discard.isEmpty };
-    assert.ok(isPlayerKind(game.round.currentMove.constructor.name));
-    this.round = {
-      number: game.round.number,
-      currentMove: {
-        name: game.round.currentMove.constructor.name,
-        allowedPlayer: {
-          id: game.round.currentMove.player.id,
-        },
-        endTime: game.round.currentMove.defaultBehavior.callTime,
-      },
-    };
-    // NOTE: even if (game.isEnded === false)
-    // all properties above should be correctly added
-    // no errors should be received while this object is being created
-    this.isGameEnded = game.isEnded;
+    this.status = game.info.status;
+    this.desk = game.desk.toJSON();
+    this.talon = game.talon.toJSON();
+    this.discard = game.discard.toJSON();
+    this.round = game.round.toJSON()
   }
 }
