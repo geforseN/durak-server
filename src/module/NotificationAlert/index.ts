26a7dec441234ace8b9edc876f576380e455932a
geforseN <semen.neb@gmail.com>
2023-09-09 21:17:41 +0700

    .

--- src/module/NotificationAlert/index.ts
+++ src/module/NotificationAlert/index.ts
@@ -2,21 +2,18 @@ import crypto from "node:crypto";
 import { type NotificationAlert as TNotificationAlert } from "@durak-game/durak-dts";
 
 export default class NotificationAlert {
-  message;
-  type;
-  durationInMS;
-  id;
+  message: TNotificationAlert["message"];
+  type: TNotificationAlert["type"];
+  durationInMS: TNotificationAlert["durationInMS"];
+  id: TNotificationAlert["id"];
 
   constructor(data: Partial<TNotificationAlert> = {}) {
     data.message ??= "Произошла ошибка";
     data.durationInMS ??= 5_000;
     data.id ??= crypto.randomUUID();
+    data.type ??= data instanceof Error ? "Error" : "Warning";
     this.message = data.message;
-    this.type = data.type
-      ? data.type
-      : data instanceof Error
-      ? "Error"
-      : "Warning";
+    this.type = data.type;
     this.durationInMS = data.durationInMS;
     this.id = data.id;
   }
