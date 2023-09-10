fe1a5581e289cd5ce24660a2afb35ed9448225ff
geforseN <semen.neb@gmail.com>
2023-09-10 22:52:56 +0700

    DurakGame: remove isEnded and make players work

--- src/module/DurakGame/DurakGame.ts
+++ src/module/DurakGame/DurakGame.ts
@@ -3,15 +3,26 @@ import type NonStartedDurakGame from "./NonStartedDurakGame.js";
 import GameRoundMoves from "./entity/GameRound/GameRoundMoves.js";
 import GameRoundDistribution from "./entity/GameRoundDistributionQueue.js";
 import NonStartedGameUser from "./entity/__Player/NonStartedGameUser.js";
-import { Card, Desk, Discard, GameRound, Players, Talon } from "./entity/index.js";
+import {
+  Card,
+  Desk,
+  Discard,
+  GameRound,
+  Players,
+  Talon,
+} from "./entity/index.js";
 import { addListenersWhichAreNeededForStartedGame } from "./socket/DurakGameSocket.handler.js";
 import {
   DurakGameWebsocketService,
   GameDeskWebsocketService,
   GameDiscardWebsocketService,
-  GameTalonWebsocketService
+  GamePlayerWebsocketService,
+  GameTalonWebsocketService,
 } from "./socket/service/index.js";
 import { pino } from "pino";
+import type { GameState } from "@durak-game/durak-dts";
+import { Player } from "./entity/Player/Player.js";
+import { Hand } from "./entity/Deck/index.js";
 
 export class InternalGameLogicError extends Error {}
 
@@ -21,8 +32,7 @@ export default class DurakGame {
     adminId: string;
     namespace: DurakGameSocket.Namespace;
     durakId?: string;
-    isStarted: true;
-    status: "starts" | "started";
+    status: GameState["status"];
   };
   readonly settings: GameSettings;
   readonly talon: Talon;
@@ -37,7 +47,6 @@ export default class DurakGame {
       target: "pino-pretty" as const,
     },
   });
-  isEnded = false;
   readonly initialPlayers: {
     id: string;
     place: number | null;
@@ -54,16 +63,29 @@ export default class DurakGame {
       ...nonStartedGame.info,
       namespace,
       status: "starts",
-      isStarted: true, // TODO remove isStarted
     };
     this.settings = {
       ...nonStartedGame.settings,
-      moveTime: nonStartedGame.settings.moveTime * 1000,
+      moveTime: 2147483647,
     };
-    this.players = nonStartedGame.usersInfo.map(
-      (info, index, array) =>
-        new NonStartedGameUser({ index, info, lobbySlotsCount: array.length }),
-    );
+    const playersData = nonStartedGame.usersInfo
+      .map(
+        (info, index, array) =>
+          new NonStartedGameUser({
+            index,
+            info,
+            lobbySlotsCount: array.length,
+            wsService: new GamePlayerWebsocketService(namespace),
+            hand: new Hand(),
+          }),
+      )
+      .map((user) => Player.create(user));
+    const players = playersData.map((data) => data[1]);
+    playersData.forEach(([leftPlayerIndex, player, rightPlayerIndex]) => {
+      player.left = players[leftPlayerIndex];
+      player.right = players[rightPlayerIndex];
+    });
+    this.players = new Players(players);
     this.talon = new Talon(
       nonStartedGame.settings,
       new GameTalonWebsocketService(namespace),
@@ -101,13 +123,14 @@ export default class DurakGame {
   }
 
   #makeInitialSuperPlayers() {
-    this.players = this.players
-      .with(this.players.get((player) => player.isAdmin).left.asDefender())
-      .with(this.players.defender.right.asAttacker().asAllowed(this));
+    const admin = this.players.get((player) => player.info.isAdmin);
+    this.players
+      .mutateWith(admin.left.asDefender())
+      .mutateWith(this.players.defender.right.asAttacker().asAllowed(this));
   }
 
   end() {
-    this.isEnded = true;
+    this.info.status = "ended";
     const [durakPlayer] = this.players;
     this.info.durakId = durakPlayer.id;
     this.#wsService.end(this);
