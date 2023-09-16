3b1583f88e60b874898b5ee0e9678123bfeb11ea
geforseN <semen.neb@gmail.com>
2023-09-16 20:24:20 +0700

    add test for buildTalon

---
+++ src/module/DurakGame/entity/Deck/Talon/buildTalon.test.ts
@@ -0,0 +1,55 @@
+import { describe, expect, it, test } from "vitest";
+import buildTalon from "./buildTalon.js";
+import type {
+  Card as CardDTO,
+  GameSettings,
+  TalonCardCount,
+} from "@durak-game/durak-dts";
+import TrumpCard from "../../Card/TrumpCard.js";
+import { Card } from "../../index.js";
+
+describe("buildTalon work good", () => {
+  test.each([0, 1, -36, 54])("throws on wrong count=%i", (count) => {
+    expect(() => {
+      buildTalon({ count: count as TalonCardCount });
+    }).toThrow();
+  });
+
+  test.each<TalonCardCount>([24, 36, 52])(
+    "correctly builded with count=%i",
+    (count) => {
+      expect(() => buildTalon({ count })).not.toThrow();
+      expect(buildTalon({ count })).length(count);
+    },
+  );
+
+  const trumpCard: CardDTO = { rank: "10", suit: "♠" };
+
+  const TALON_SETTINGS: GameSettings["talon"][] = [
+    { count: 24, trumpCard },
+    { count: 36, trumpCard },
+    { count: 52, trumpCard },
+  ];
+
+  TALON_SETTINGS.forEach(({ count, trumpCard }) => {
+    expect(() => buildTalon({ count, trumpCard })).not.toThrow();
+    const talonCards = buildTalon({ count, trumpCard });
+    describe("given trump card are correct in talon cards", () => {
+      it("has trump card as most bottom card", () => {
+        expect(talonCards[0]).toContain(trumpCard);
+      });
+
+      it("is instance of TrumpCard", () => {
+        expect(talonCards[0]).toBeInstanceOf(TrumpCard);
+      });
+
+      it(`deck has right amount of trump cards=${
+        count / Card.suits.length
+      }`, () => {
+        expect(talonCards.filter((card) => card.isTrump)).length(
+          count / Card.suits.length,
+        );
+      });
+    });
+  });
+});
