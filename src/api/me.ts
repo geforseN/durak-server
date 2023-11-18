6617542c42069c013531b0332876c17d3ba6ee27
geforseN <semen.neb@gmail.com>
2023-11-18 21:25:23 +0700

    change method syntax

--- src/api/me.ts
+++ src/api/me.ts
@@ -4,7 +4,7 @@ export default async function getMe(fastify: FastifyInstance) {
   return fastify.route({
     method: "GET",
     url: "/me",
-    handler: async function (request, reply) {
+    async handler(request, reply) {
       this.log.info({ user: request.session.user });
       return reply.send({ user: request.session.user });
     },
