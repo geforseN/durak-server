5467ceb1f9ad08709ea3a5210ac1c69319c9b29b
geforseN <geforsenjs@gmail.com>
2024-12-01 16:06:19 +0700

    chore(config): [eslint] custom no-unused-vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,21 @@ export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   vitest.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
 );
