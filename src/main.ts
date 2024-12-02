5d2961409555dc3404f6ec12a5cd79fda358a6bb
geforseN <geforsenjs@gmail.com>
2024-12-02 20:31:35 +0700

    feat: add FastifyListerOptionsSchema

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import Fastify from "fastify";
 import createFastify from "./app.js";
 import consola from "consola";
+import { z } from "zod";
+
+const FastifyListerOptionsSchema = z.object({
+  PORT: z.coerce.number().int(),
+  HOST: z.string(),
+}).transform(({ PORT, HOST }) => ({
+  port: PORT,
+  host: HOST
+}))
 
 const start = async () => {
-  const port = 10000;
+  const fastifyListenOptions = FastifyListerOptionsSchema.parse(process.env);
   const log = consola.withTag("Fastify");
   try {
     log.info("Creating...");
     const fastify = await createFastify(Fastify({ logger: true }));
     log.info("Trying to listen...");
-    const address = await fastify.listen({ port });
+    const address = await fastify.listen(fastifyListenOptions);
     log.info("Listening on address ", address);
   } catch (reason) {
     log.error(`Failed to start`, { reason });
