637a56a47c0b7522563857b9316a2ccc0ca9263a
geforseN <geforsenjs@gmail.com>
2024-12-09 00:15:30 +0700

    fix: SocketStream is not imported
    https://github.com/fastify/fastify-websocket/pull/295
    https://github.com/fastify/fastify-websocket/issues/289

--- src/module/Chat/initializeChatNamespace.ts
+++ src/module/Chat/initializeChatNamespace.ts
@@ -1,40 +1,21 @@
-import type { SocketStream } from "@fastify/websocket";
+import type { WebSocket } from "@fastify/websocket";
 import type { FastifyRequest } from "fastify";
-import type { WebSocket } from "ws";
-import {
-  SocketsStore,
-  defaultListeners,
-  type CustomWebsocketEvent,
-} from "../../ws/index.js";
+import { SocketsStore, defaultListeners } from "../../ws/index.js";
 import { Chat } from "./entity/index.js";
-
-export interface AdditionalListeners {
-  addListener(
-    event: "socket",
-    listener: (this: WebSocket, event: CustomWebsocketEvent) => void,
-  ): this;
-  on(
-    event: "socket",
-    listener: (this: WebSocket, event: CustomWebsocketEvent) => void,
-  ): this;
-}
 
 export default function initializeChat() {
   const socketsStore = new SocketsStore();
   const chat = new Chat({ socketsStore });
 
-  return function getChatContext(
-    connection: SocketStream & { socket: WebSocket & AdditionalListeners },
-    request: FastifyRequest,
-  ) {
-    socketsStore.add(connection.socket);
-    socketsStore.room(request.session.user.id).add(connection.socket);
-    connection.socket
+  return function getChatContext(socket: WebSocket, request: FastifyRequest) {
+    socketsStore.add(socket);
+    socketsStore.room(request.session.user.id).add(socket);
+    socket
       .addListener("socket", defaultListeners.socket)
       .addListener("message", defaultListeners.message);
     return {
       sender: request.session.user,
-      socket: connection.socket,
+      socket,
       chat,
       socketsStore,
     };
