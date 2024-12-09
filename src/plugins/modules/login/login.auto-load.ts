7c4149f153f3cd63e4b4064b79c525ab0da615d4
geforseN <geforsenjs@gmail.com>
2024-12-09 22:19:04 +0700

    refactor(api): [login] change session mutation condition

--- src/plugins/modules/login/login.auto-load.ts
+++ src/plugins/modules/login/login.auto-load.ts
@@ -26,7 +26,7 @@ export default <FastifyPluginAsyncZod>async function (fastify) {
     method: "POST",
     url: "/api/auth/login",
     async handler(request, reply) {
-      if (typeof request.session.user?.isAnonymous === "undefined") {
+      if (request.session.user === undefined) {
         await this.mutateSessionWithAnonymousUser(request);
       }
       return reply.redirect(redirectUrl);
