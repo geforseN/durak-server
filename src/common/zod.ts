b5e9046fa1f935041c391366c3d89ecc9365b397
geforseN <geforsenjs@gmail.com>
2024-12-09 20:34:50 +0700

    chore(common): remove stringToBoolean

--- src/common/zod.ts
+++ src/common/zod.ts
@@ -1,13 +1,3 @@
-import raise from "./raise.js";
-
-export function stringToBoolean(str: string) {
-  return str === "true"
-    ? true
-    : str === "false"
-      ? false
-      : raise('value must be "true" or "false"');
-}
-
 export function stringToArray(str: string) {
   return str.split(",").map((value) => value.trim());
 }
