a053d97c928fc672b2883ef8a88fbb8cf5eb0296
geforseN <geforsenjs@gmail.com>
2024-12-09 16:29:21 +0700

    feat: [main] auto register core plugins
    issue: can not login,  application/x-www-form-urlencoded not supported
    fixed with `encapsulate: false`
    
    also:
    add temporary logs
    fix socket-io ts error

---
+++ src/plugins/app.auto-load.ts
@@ -0,0 +1,23 @@
+import { fastifyCookie } from "@fastify/cookie";
+import { fastifyCors } from "@fastify/cors";
+import { fastifyFormbody } from "@fastify/formbody";
+import { fastifySession } from "@fastify/session";
+import { fastifyWebsocket } from "@fastify/websocket";
+import { getFastifySessionSettings } from "../config/fastify.js";
+import { env, sessionStore } from "../config/index.js";
+import type { FastifyInstance } from "fastify";
+
+export default async function myPlugin(app: FastifyInstance) {
+  console.log("start app.auto-load");
+  await app
+    .register(fastifyCors, {
+      origin: env.CORS_ORIGIN,
+      methods: ["GET", "POST"],
+      credentials: true,
+    })
+    .register(fastifyFormbody)
+    .register(fastifyCookie)
+    .register(fastifySession, getFastifySessionSettings(env, sessionStore))
+    .register(fastifyWebsocket);
+  console.log("end app.auto-load");
+}
