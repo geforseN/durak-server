a7a0e8a7bb5db5a3ba74a4506846b4d3d85793b4
geforseN <geforsenjs@gmail.com>
2024-12-09 20:38:12 +0700

    refactor(common): [raise] alias to assert.fail

--- src/common/raise.ts
+++ src/common/raise.ts
@@ -1,3 +1,5 @@
-export default function raise(err: Error | string = new Error()): never {
-  throw typeof err === "string" ? new Error(err) : err;
-}
+import assert from "node:assert";
+
+const raise = assert.fail;
+
+export default raise;
