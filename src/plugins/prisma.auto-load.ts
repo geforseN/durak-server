a94f1e34c95a73a6e3a42a4eae836c0e0c2e4e4f
geforseN <geforsenjs@gmail.com>
2024-12-10 17:42:57 +0700

    chore: remove zod plugin type import

--- src/plugins/prisma.auto-load.ts
+++ src/plugins/prisma.auto-load.ts
@@ -1,4 +1,3 @@
-import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import fastifyPrisma from "@joggr/fastify-prisma";
 
 export default <FastifyPluginAsyncZod>async function (app) {
