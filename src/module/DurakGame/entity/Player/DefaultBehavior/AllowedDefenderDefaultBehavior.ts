60ae21a4563b34a7e5d81557b4dd56ca1ab514b7
geforseN <geforsenjs@gmail.com>
2024-12-26 00:50:39 +0700

    refactor: [AllowedDefenderDefaultBehavior] just make stop move

--- src/module/DurakGame/entity/Player/DefaultBehavior/AllowedDefenderDefaultBehavior.ts
+++ src/module/DurakGame/entity/Player/DefaultBehavior/AllowedDefenderDefaultBehavior.ts
@@ -1,10 +1,6 @@
-import assert from "node:assert";
-
-import type { Card } from "@/module/DurakGame/entity/index.js";
 import type { AllowedDefender } from "@/module/DurakGame/entity/Player/AllowedDefender.js";
 
 import DefaultBehavior from "@/module/DurakGame/entity/Player/DefaultBehavior/DefaultBehavior.js";
-import getDefenseStrategy from "@/module/DurakGame/entity/Player/DefaultBehavior/getDefenseStrategy.js";
 
 export class AllowedDefenderDefaultBehavior extends DefaultBehavior<AllowedDefender> {
   constructor(
@@ -16,44 +12,7 @@ export class AllowedDefenderDefaultBehavior extends DefaultBehavior<AllowedDefen
   }
 
   async makeMove() {
-    if (!this.shouldBeCalled) {
-      return console.log("defaultBehavior DEFENDER: fast return");
-    }
-    // TODO should remove if code is not trash
-    assert.ok(this.allowedPlayer.isAllowed());
-    try {
-      try {
-        const card = await Promise.any(
-          [...this.allowedPlayer.hand].map(
-            async (card: Card) => {
-              await this.allowedPlayer.ensureCanMakeTransferMove(card);
-              return card;
-            },
-          ),
-        );
-        assert.ok(card);
-        return await this.allowedPlayer.makeInsertMove(
-          card,
-          this.game.desk.randomEmptySlot,
-        );
-      } catch {
-        const { defenseStrategy } = getDefenseStrategy(
-          [...this.allowedPlayer.hand],
-          this.game.desk.unbeatenSlots.cards,
-        );
-        const { attackCard, defendCard } = defenseStrategy[0];
-        const slot = [...this.game.desk].find(
-          (slot) =>
-            slot.attackCard?.rank === attackCard.rank &&
-            slot.attackCard.suit === attackCard.suit,
-        );
-        assert.ok(slot);
-        return await this.allowedPlayer.makeInsertMove(defendCard, slot);
-      }
-    } catch (error) {
-      console.log("defaultBehavior DEFENDER: stopMove", error);
-      return this.allowedPlayer.makeStopMove();
-    }
+    return this.allowedPlayer.makeStopMove();
   }
 }
 
