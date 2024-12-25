ba02a1172c5f2fefb79b5f12d92d82f922909b8d
geforseN <geforsenjs@gmail.com>
2024-12-26 01:27:58 +0700

    chore: [AllowedDefender] delete default behavior

--- src/module/DurakGame/entity/Player/AllowedDefender.ts
+++ src/module/DurakGame/entity/Player/AllowedDefender.ts
@@ -7,16 +7,12 @@ import {
   StopDefenseMove,
 } from "@/module/DurakGame/entity/GameMove/index.js";
 import { AllowedSuperPlayer } from "@/module/DurakGame/entity/Player/AllowedSuperPlayer.abstract.js";
-import AllowedDefenderDefaultBehavior from "@/module/DurakGame/entity/Player/DefaultBehavior/AllowedDefenderDefaultBehavior.js";
 import { Defender } from "@/module/DurakGame/entity/Player/Defender.js";
 import { type SuperPlayer } from "@/module/DurakGame/entity/Player/SuperPlayer.abstract.js";
 
 export class AllowedDefender extends AllowedSuperPlayer {
-  defaultBehavior: AllowedDefenderDefaultBehavior;
-
   constructor(superPlayer: SuperPlayer, game: DurakGame) {
     super(superPlayer, game);
-    this.defaultBehavior = new AllowedDefenderDefaultBehavior(this);
   }
 
   asAllowed(): AllowedDefender {
@@ -24,21 +20,15 @@ export class AllowedDefender extends AllowedSuperPlayer {
   }
 
   asAllowedAgain(): AllowedDefender {
-    this.defaultBehavior.shouldBeCalled = false;
-    this.defaultBehavior.clearTimeout();
     return new AllowedDefender(this, this.game);
   }
 
   asDisallowed(): Defender {
-    this.defaultBehavior.shouldBeCalled = false;
-    this.defaultBehavior.clearTimeout();
     return new Defender(this);
   }
 
   async makeInsertMove(card: Card, slot: DeskSlot) {
     await slot.ensureCanBeDefended(card);
-    this.defaultBehavior.shouldBeCalled = false;
-    this.defaultBehavior.clearTimeout();
     return new InsertDefendCardMove(this.game, this, {
       card,
       slot,
@@ -46,8 +36,6 @@ export class AllowedDefender extends AllowedSuperPlayer {
   }
 
   makeStopMove() {
-    this.defaultBehavior.shouldBeCalled = false;
-    this.defaultBehavior.clearTimeout();
     if (!this.game.desk.isDefended) {
       return new DefenderGaveUpMove(this.game, this);
     }
