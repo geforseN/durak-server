123b2d141ca54aa4cab8233fe929443f3b18ac17
geforseN <geforsenjs@gmail.com>
2024-12-26 01:56:32 +0700

    fix: [StopDefenseMove] isInsertMove bad type

--- src/module/DurakGame/entity/GameMove/StopDefenseMove.ts
+++ src/module/DurakGame/entity/GameMove/StopDefenseMove.ts
@@ -3,13 +3,14 @@ import type { AllowedDefender } from "@/module/DurakGame/entity/Player/AllowedDe
 
 import FailedDefense from "@/module/DurakGame/entity/DefenseEnding/FailedDefense.js";
 import GameMove from "@/module/DurakGame/entity/GameMove/GameMove.abstract.js";
+import type InsertGameMove from "@/module/DurakGame/entity/GameMove/InsertGameMove.abstract.js";
 
 export default class StopDefenseMove extends GameMove<AllowedDefender> {
   constructor(game: DurakGame, performer: AllowedDefender) {
     super(game, performer);
   }
 
-  override isInsertMove(): boolean {
+  override isInsertMove(): this is InsertGameMove<AllowedDefender> {
     return false;
   }
 
