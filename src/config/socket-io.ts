ee264b9bf8f569e91026d32b1a69759931606da4
geforseN <geforsenjs@gmail.com>
2024-12-26 15:30:58 +0700

    refactor: make createSocketIoServer async
    remove mutateSocketData middleware
    update imports
    add log argument
    lazy load dev only package

--- src/config/socket-io.ts
+++ src/config/socket-io.ts
@@ -1,20 +1,21 @@
 import type { Server } from "socket.io";
-import { instrument } from "@socket.io/admin-ui";
-import { DurakGameSocket } from "@durak-game/durak-dts";
+import type { Logger } from "pino";
+import type { DurakGameSocket } from "@durak-game/durak-dts";
 import durakGameSocketHandler from "@/module/DurakGame/socket/DurakGameSocket.handler.js";
-import { SessionStore } from "@fastify/session";
-import { mutateSocketData } from "@/module/DurakGame/socket/mutateSocketData.js";
+import { isDevelopment } from "std-env";
 
-export function createSocketIoServer(
-  io: Server,
-  sessionStore: SessionStore,
-) {
+export async function createSocketIoServer(io: Server, log: Logger) {
   const gamesNamespace: DurakGameSocket.Namespace = io.of(
     /^\/game\/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/,
   );
-  gamesNamespace.use((socket, next) => {
-    mutateSocketData.call(null, socket, next, sessionStore);
-  });
-  gamesNamespace.on("connection", durakGameSocketHandler);
-  instrument(io, { auth: false, mode: "development" });
+  gamesNamespace.on(
+    "connection",
+    function (this: DurakGameSocket.Namespace, socket) {
+      durakGameSocketHandler.call(this, socket, log);
+    },
+  );
+  if (isDevelopment) {
+    const { instrument } = await import("@socket.io/admin-ui");
+    instrument(io, { auth: false, mode: "development" });
+  }
 }
--- src/plugins/socket-io.auto-load.ts
+++ src/plugins/socket-io.auto-load.ts
@@ -1,7 +1,7 @@
 import assert from "node:assert";
 import fastifySocketIO from "fastify-socket.io";
 import type SocketIO from "socket.io";
-import { createSocketIoServer, env, sessionStore } from "@/config/index.js";
+import { createSocketIoServer, env } from "@/config/index.js";
 
 export default <FastifyPluginAsyncZod>async function (app) {
   app
@@ -13,9 +13,9 @@ export default <FastifyPluginAsyncZod>async function (app) {
       },
     })
     .ready()
-    .then(() => {
+    .then(async () => {
       assert("io" in app);
-      createSocketIoServer(<SocketIO.Server>app.io, sessionStore);
+      await createSocketIoServer(<SocketIO.Server>app.io, app.log);
       app.log.trace('Loaded `socket-io` plugin.');
     });
 };
