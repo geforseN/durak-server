0b76e42e6549f3dc43b22b648f5014aad7d87e6a
geforseN <geforsenjs@gmail.com>
2025-02-01 19:43:53 +0700

    chore(durak-game): [non-started] init

--- src/module/DurakGame/NonStartedDurakGame.ts
+++ src/module/DurakGame/NonStartedDurakGame.ts
@@ -1,58 +1,24 @@
 import type { GameSettings } from "@durak-game/durak-dts";
-import type { SessionUser } from "@/plugins/modules/login/login.instance-decorators.js";
-import type WebSocket from "ws";
-
-class NonConnectedPlayer {}
-
-class PlayerConnection {
-  constructor(
-    public sessionUser: SessionUser,
-    public socket: WebSocket,
-  ) {}
-}
-
-class PlayersConnections<K extends string = SessionUser["id"]> {
-  constructor(
-    readonly map: Map<K, Set<PlayerConnection>>,
-    readonly sizeGoal: number,
-    readonly onReady: () => void,
-  ) {}
-
-  add(connection: PlayerConnection) {
-    const key = connection.sessionUser.id as K;
-    const connections = this.map.get(key);
-    if (connections) {
-      connections.add(connection);
-    } else {
-      this.map.set(key, new Set([connection]));
-      if (this.sizeGoal === this.map.size) {
-        this.onReady();
-      }
-    }
-  }
-
-  get(key: K) {
-    return this.map.get(key);
-  }
-
-  require(
-    key: K,
-    makeError = () =>
-      new Error(`Failed to find connection with user id = ${key}`),
-  ) {
-    const connections = this.get(key);
-    if (!connections) {
-      throw makeError();
-    }
-    return connections;
-  }
-}
+import type PlayerWebSocketConnection from "@/module/DurakGame/player-websocket-connection.js";
+import type NonStartedGamePlayersConnections from "@/module/DurakGame/non-started-game-players-connections.js";
 
 export default class NonStartedDurakGame {
+  readonly #connections: NonStartedGamePlayersConnections;
+
   constructor(
-    public id: string,
-    public setting: GameSettings,
-    public connections: PlayersConnections,
-    readonly onReady: () => void,
-  ) {}
+    public readonly id: string,
+    public readonly setting: GameSettings,
+    connections: NonStartedGamePlayersConnections,
+  ) {
+    this.#connections = connections;
+  }
+
+  connect(connection: PlayerWebSocketConnection) {
+    // TODO: ensure in allowed ids, if not than add connection as guest
+    this.#connections.add(connection);
+  }
+
+  sleepUntilCanBecomeReady() {
+    return this.#connections.sleepUntilAllConnected();
+  }
 }
