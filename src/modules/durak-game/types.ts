0de6293f93f0925e19fe731e749d6ff01fee0754
geforseN <geforsenjs@gmail.com>
2025-02-01 19:41:47 +0700

    chore(durak-game): [types] init

---
+++ src/modules/durak-game/types.ts
@@ -0,0 +1,4 @@
+import type DurakGame from "@/module/DurakGame/DurakGame.js";
+import type NonStartedDurakGame from "@/module/DurakGame/NonStartedDurakGame.js";
+
+export type Game = NonStartedDurakGame | DurakGame;
