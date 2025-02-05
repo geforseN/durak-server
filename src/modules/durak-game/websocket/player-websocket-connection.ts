301e4a786c16054be2b01dc18c64bbd66bf4987b
geforseN <geforsenjs@gmail.com>
2025-02-05 20:42:29 +0700

    chore(durak-game): [player-websocket-connection] move to modules

--- src/modules/durak-game/websocket/player-websocket-connection.ts
+++ src/modules/durak-game/websocket/player-websocket-connection.ts
@@ -4,6 +4,18 @@ import type { IConnection } from "@/modules/abstract/connections.js";
 type PlayerId = string;
 
 export default class PlayerWebSocketConnection implements IConnection {
+  applyTo(player: unknown) {
+    throw new Error("Method not implemented.");
+    if (player)
+      this.websocket.addEventListener("message", () => {
+        if (!player) {
+          // guest can not change game state so
+          // guest can not send message to change state
+          return;
+        }
+      });
+  }
+
   constructor(
     private readonly playerId: PlayerId,
     private readonly websocket: WebSocket,
