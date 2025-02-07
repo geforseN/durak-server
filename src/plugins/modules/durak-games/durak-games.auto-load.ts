f51614d10ae23af668f8a70841512bf90b4130df
geforseN <geforsenjs@gmail.com>
2025-02-07 21:49:11 +0700

    refactor(plugins): [durak-games] use new code

--- src/plugins/modules/durak-games/durak-games.auto-load.ts
+++ src/plugins/modules/durak-games/durak-games.auto-load.ts
@@ -1,65 +1,28 @@
 import assert from "node:assert";
 import z from "zod";
-import type WebSocket from "ws";
-import durakGamesStore from "@/modules/durak-game/durak-games-store-singleton.js";
-import DurakGame from "@/module/DurakGame/DurakGame.js";
-import NonStartedDurakGame from "@/module/DurakGame/NonStartedDurakGame.js";
-import { GameRestoreStateEventSchema } from "@/utils/durak-game-state-restore-schema.js";
-import findInstantlyAndAfterTimeout from "@/utils/timeout/find-instantly-and-after-timeout.js";
+import durakGamesStore from "@/modules/durak-game/store/instance.js";
+import PlayerWebSocketConnection from "@/modules/durak-game/websocket/player-websocket-connection.js";
+import {
+  findGameWithTimeout,
+  sleepUntilGameStart,
+} from "@/modules/durak-game/store/utils.js";
+import type { FastifyRequest } from "fastify";
+import { makeStartedGame } from "@/modules/durak-game/started/make.js";
 
-async function findGameWithTimeout(
-  gameId: string,
-  timeout: number,
-  log: (...args: [message: string, ...args: unknown[]]) => void,
-) {
-  return await findInstantlyAndAfterTimeout(
-    timeout,
-    () => durakGamesStore.get(gameId),
-    {
-      onInstantFound() {
-        log("found game with id %s", gameId);
-      },
-      onAfterTimeoutFound() {
-        log("found game with id %s after timeout %s", gameId, timeout);
-      },
-      onNotFound() {
-        log("game with id %s not found even after timeout %s", gameId, timeout);
-      },
-    },
-  );
-}
-
-function isStartedGame(game: DurakGame | NonStartedDurakGame) {
-  return game instanceof DurakGame;
-}
-
-class GameStateRestoreEvent {
-  constructor(
-    readonly game: DurakGame,
-    readonly playerId: string,
-  ) {}
-
-  toString() {
-    const { game, playerId } = this;
-    const player = game.players.find((player) => player.id === playerId);
-    let self, enemies;
-    if (player) {
-      self = player.toSelf();
-      enemies = player.enemies;
-    }
-    const state = GameRestoreStateEventSchema.parse({
-      event: "game::state::restore",
-      payload: {
-        ...game.toJSON(),
-        self,
-        enemies,
-      },
-    });
-    return JSON.stringify(state);
+declare module "fastify" {
+  interface FastifyRequest {
+    requireSessionUserId(): string;
   }
 }
 
+function requireSessionUserId(this: FastifyRequest) {
+  assert.ok(this.session.user, "session has no user");
+  const playerId = this.session.user.id;
+  return playerId;
+}
+
 export default <FastifyPluginAsyncZod>async function (app) {
+  app.decorateRequest("requireSessionUserId", requireSessionUserId);
   app.get(
     "/games/:gameId",
     {
@@ -72,72 +35,26 @@ export default <FastifyPluginAsyncZod>async function (app) {
     },
     async function (socket, request) {
       const { gameId } = request.params;
-      const game = await findGameWithTimeout(gameId, 1500, this.log.info);
+      const game = await findGameWithTimeout(
+        durakGamesStore,
+        gameId,
+        1500,
+        this.log.info.bind(this.log),
+      );
       if (!game) {
+        // TODO: send that such game does not exist;
         return;
       }
-      const asyncGame = Promise.withResolvers<{
-        game: DurakGame;
-        player: {
-          id: string;
-          sockets: WebSocket[];
-        };
-      }>();
-      assert.ok(request.session.user, "session has no user");
-      const playerId = request.session.user.id;
-      this.log.info("connected to game", { gameId, playerId });
-      asyncGame.promise.then(({ game, player }) => {
-        for (const socket of player.sockets) {
-          const playerId = player.id;
-          this.log.info("game ready, adding socket listeners", {
-            gameId,
-            playerId,
-          });
-          // socket.join(playerId);
-          this.log.info('sending "game::state::restore"', { gameId, playerId });
-          socket.send(new GameStateRestoreEvent(game, playerId).toString());
-        }
-      });
-      if (isStartedGame(game)) {
-        this.log.info("game already started", { gameId });
-        asyncGame.resolve({
-          game,
-          player: {
-            id: playerId,
-            sockets: [socket],
-          },
-        });
-      } else if (game instanceof NonStartedDurakGame) {
-        this.log.info("game is not started yet", { gameId });
-        game.connections.add(playerId, socket);
-        if (!game.isAllPlayersConnected) {
-          return this.log.info("not all players connected yet", { gameId });
-        }
-        this.log.info("all players connected", { gameId });
-        this.log.info("all players connected", { gameId });
-        const storeGame = durakGamesStore.get(game.id);
-        let startedGame: DurakGame;
-        if (storeGame instanceof DurakGame) {
-          this.log.warn('race condition "game" in "durakGamesStore"');
-          startedGame = storeGame;
-        } else if (storeGame) {
-          this.log.info("adding DurakGame", { gameId });
-          startedGame = new DurakGame(game.id, game.setting);
-          durakGamesStore.set(gameId, startedGame);
-          this.log.info("set DurakGame in store", { gameId });
-        } else {
-          throw new Error("storeGame is not DurakGame or NonStartedDurakGame");
-        }
-        const playerSocketsSet = game.sockets.get(playerId);
-        assert.ok(playerSocketsSet);
-        asyncGame.resolve({
-          game: startedGame,
-          player: {
-            id: playerId,
-            sockets: [...playerSocketsSet],
-          },
-        });
-      }
+      const playerId = request.requireSessionUserId();
+      this.log.info("playerId=%s", playerId);
+      const connection = new PlayerWebSocketConnection(playerId, socket);
+      const startedGame = await sleepUntilGameStart(
+        durakGamesStore,
+        game,
+        connection,
+        makeStartedGame,
+      );
+      startedGame.connect(connection);
     },
   );
 };
