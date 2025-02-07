e9babec8dd0ef4340ec2c4014ac7092d0e65c522
geforseN <geforsenjs@gmail.com>
2025-02-07 21:38:43 +0700

    refactor(utils): [MapEntityStores] change
    add ConstructableEntity
    remove E, use InstanceType<C>
    fix #map type

--- src/utils/map-entity-stores.ts
+++ src/utils/map-entity-stores.ts
@@ -1,31 +1,36 @@
 import type { Entity, EntityStore } from "@/types/entity-store.js";
 
-export class MapEntityStores<
-  C extends { new (...args: any[]): Entity },
-  E extends InstanceType<C> = InstanceType<C>,
-> {
-  #map;
+type ConstructableEntity = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any -- have to use any, unknown[] will not work
+  new (...args: any[]): Entity;
+};
 
-  constructor(map: Map<C, EntityStore<Entity>>) {
+export class MapEntityStores<C extends ConstructableEntity> {
+  #map: Map<C, EntityStore<InstanceType<C>>>;
+
+  constructor(map: Map<C, EntityStore<InstanceType<C>>>) {
     this.#map = map;
   }
 
-  static create<CA extends { new (...args: any[]): Entity }[]>(values: {
+  static create<CA extends ConstructableEntity[]>(values: {
     [K in keyof CA]: { For: CA[K]; store: EntityStore<InstanceType<CA[K]>> };
   }) {
     return new MapEntityStores<CA[number]>(
+      // @ts-expect-error Type 'Entity' is not assignable to type 'InstanceType<CA[number]>'
       new Map(values.map((value) => [value.For, value.store])),
     );
   }
 
-  get(id: E["id"]) {
+  get(id: InstanceType<C>["id"]) {
     for (const store of this.#map.values()) {
       const value = store.get(id);
-      if (value) return value as E;
+      if (value) {
+        return value;
+      }
     }
   }
 
-  require(id: E["id"]) {
+  require(id: InstanceType<C>["id"]) {
     const entity = this.get(id);
     if (!entity) {
       throw new Error(`Unknown entity with id = ${id}`);
@@ -33,7 +38,7 @@ export class MapEntityStores<
     return entity;
   }
 
-  set(entity: E) {
+  set(entity: InstanceType<C>) {
     const Class = this.#map.keys().find((Class) => entity instanceof Class);
     if (!Class) {
       throw new Error("Unknown entity");
@@ -45,7 +50,7 @@ export class MapEntityStores<
     store.set(entity);
   }
 
-  delete(value: E | E["id"]) {
+  delete(value: InstanceType<C> | InstanceType<C>["id"]) {
     const store = this.#map.values().find((store) => store.has(value));
     if (!store) {
       throw new Error("Unknown entity value");
@@ -53,7 +58,7 @@ export class MapEntityStores<
     store.delete(value);
   }
 
-  has(value: E | E["id"]): boolean {
+  has(value: InstanceType<C> | InstanceType<C>["id"]): boolean {
     return this.#map.values().some((store) => store.has(value));
   }
 }
